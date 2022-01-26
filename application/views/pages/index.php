1565fd1f5364a141832def66574c18fe26a49296
99061142 <99061142@mydavinci.nl>
2022-01-27 00:00:33 +0100

    Updated the homepage + styling, to work better with the board

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,39 +1,32 @@
 <div class="d-flex justify-content-around my-2">
-    <div>
-        <p>
-            <i class="fas fa-arrow-right"></i>
-            Starting point
-        </p>
-    </div>
+    <p>
+        <i class="fas fa-arrow-right"></i>
+        Starting point
+    </p>
 
-    <div>
-        <p>
-            <i class="fas fa-home"></i>
-            End point
-        </p>
-    </div>
+    <p>
+        <i class="fas fa-home"></i>
+        End point
+    </p>
 
-    <div>
-        <p>
-            <i class="fas fa-square"></i>
-            Walls
-        </p>
-    </div>
+    <p>    
+        <i class="fas fa-square"></i>
+        Walls
+    </p>
 
-        <p>
-            <i class="fas fa-square text-warning"></i>
-            Fastest route
-        </p>
-    </div>
+    <p>
+        <i class="fas fa-square text-warning"></i>
+        Fastest route
+    </p>
 </div>
 
 <p class="text-center">* Information about the algorithm *</p>
 
-<div class="my-2" id="nodes">
-    <?php for($i = 0; $i <= 5; $i++){ ?>
+<div class="d-flex justify-content-center my-2" id="nodes">
+    <?php for($row = 0; $row < 75; $row++){ ?>
         <div>
-            <?php for($x = 0; $x <= 5; $x++){ ?>
-                <div class="node"></div>
+            <?php for($i = 0; $i < 32; $i++){ ?>
+                <div class="node border border-dark"></div>
             <?php } ?>
         </div>
     <?php } ?>
