2e7e65237084a1b313793ece9851939b58da600d
99061142 <99061142@mydavinci.nl>
2022-01-31 21:32:42 +0100

    Deleted the function where an BFS algorithm gets runned

--- js/board.js
+++ js/board.js
@@ -4,10 +4,12 @@ function make_path(){
 
     // If the start / end node is on the board
     if(start_node != null && end_node != null){
+        run_button_activation()
+
         const board = make_board(); // Make the board
         const start = get_position(start_node); // Get the position of the start node
-        const end = get_position(end_node); // Get the position of the end node 
-        path(board, start, end); // Make the path
+        const end = get_position(end_node); // Get the position of the end node     
+        path_bfs(board, start, end); // Make the path
     }
 }
 
@@ -80,46 +82,4 @@ function found_node(row, col){
 // Wait an x amount of milliseconds
 function sleep(time){
     return new Promise(resolve => setTimeout(resolve, time));
-}
-
-
-// Check each node to until the ending node is found
-async function path(board, start, end){
-    const positions = [start]; // Positions of the node(s)
-    const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // Up, right, down and left
-
-    var ending_found = false;
-
-    // Loop through every position
-    for(const [row, col] of positions){
-        // Check every direction
-        for(const [direction_row, direction_col] of directions){
-            const row_num = row + direction_row; // New row of the node
-            const col_num = col + direction_col; // New column of the node
-
-            // Check if the ending position is found
-            if(row_num == end[0] && col_num == end[1]){
-                ending_found = true;
-                break;
-            }
-
-            // If the block is an empty node
-            if(board[row_num] && board[row_num][col_num] === 0){
-                found_node(row_num, col_num); // Change the backgroundcolor
-
-                positions.push([row_num, col_num]); // Let the node be checked the next round
-                
-                board[row_num][col_num] = 1; // Change the node to the wall value
-
-                if(speed > 0){
-                    await sleep(speed); // Wait an x amount of milliseconds
-                }
-            }
-        }
-        
-        // If the ending position is found
-        if(ending_found){
-            break;
-        }
-    }
 }
