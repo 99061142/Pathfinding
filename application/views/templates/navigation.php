e8aa3314481c4147d602353d5eefd9909ee0dd34
99061142 <99061142@mydavinci.nl>
2022-01-31 17:11:42 +0100

    Changed/added onclick attributes and added a speed dropdown
    * Deleted a few unnecessary onclick attributes.
    
    * Added onclick event for the board type.
    
    * Added a dropdown menu to choose the speed to find the fastest path.

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand">Pathfinding</a> <!-- /Title -->
+    <a class="navbar-brand">Pathfinding</a>
 
     <!-- Navigation toggler -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
 
     <div class="collapse navbar-collapse mt-1" id="navbarNavDropdown">
         <ul class="navbar-nav">
-            <!-- Algorithms -->
+            <!-- All the pathfinding algorithm options -->
             <li class="nav-item dropdown">
                 <a class="dropdown-toggle" id="changeDropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Algorithms</a>
                 <div class="dropdown-menu mt-4" id="algorithm-dropdown" aria-labelledby="changeDropdownMenu">
@@ -18,17 +18,17 @@
                     <button class="dropdown-item">AStar</button>
                 </div>
             </li>
-            <!-- /Algorithms -->
+            <!-- All the pathfinding algorithm options -->
 
-            <!-- Random layouts -->
+            <!-- Choose the layout of the board -->
             <li class="nav-item dropdown">
                 <a class="dropdown-toggle" id="changeDropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layouts</a>
                 <div class="dropdown-menu mt-4" id="algorithm-dropdown" aria-labelledby="changeDropdownMenu">
-                    <button class="dropdown-item" onclick="">Self</button>
-                    <button class="dropdown-item" onclick="">Maze</button>
+                    <button class="dropdown-item" onclick=clear_board()>Normal</button>
+                    <button class="dropdown-item">Maze</button>
                 </div>
             </li>
-            <!-- /Random layouts -->
+            <!-- Choose the layout of the board -->
 
 
             <!-- Run the algorithm -->
@@ -37,23 +37,36 @@
             </li>
             <!-- /Run the algorithm -->
 
-            <!-- Reset all the nodes on the board -->
+            <!-- Reset all the changed nodes on the board -->
             <li class="nav-item">
                 <button class="buttton-as-anchor" onclick="clear_board()">Clear board</button>
             </li>
-            <!-- /Reset all the nodes on the board -->
+            <!-- /Reset all the changed nodes on the board -->
 
             <!-- Reset all the walls on the board -->
             <li class="nav-item">
-                <button class="buttton-as-anchor" onclick="clear_node_background('walls')">Clear walls</button>
+                <button class="buttton-as-anchor" onclick="clear_specific_nodes('walls')">Clear walls</button>
             </li>
             <!-- /Reset all the walls on the board -->
 
             <!-- Clear the path the algorithm made -->
             <li class="nav-item">
-                <button class="buttton-as-anchor" onclick="">Clear algorithm path</button>
+                <button class="buttton-as-anchor" onclick="clear_specific_nodes('path')">Clear algorithm path</button>
             </li>
             <!-- /Clear the path the algorithm made -->
+
+
+            <!-- Speed to find the path -->
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" id="changeDropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Speed</a>
+                <div class="dropdown-menu mt-4" id="speed-dropdown" aria-labelledby="changeDropdownMenu">
+                    <button class="dropdown-item">Slow</button>
+                    <button class="dropdown-item">Normal</button>
+                    <button class="dropdown-item">Fast</button>
+                    <button class="dropdown-item">Instant</button>
+                </div>
+            </li>
+            <!-- /Speed to find the path -->
         </ul>
     </div>
 </nav>  
