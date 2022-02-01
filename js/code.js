a7f47f39638d5b2f3a76c6447b132f2628123acb
99061142 <99061142@mydavinci.nl>
2022-02-01 22:09:40 +0100

    Deleted a few unnecessary code

--- js/code.js
+++ js/code.js
@@ -1,33 +1,24 @@
-var find_path = true // If the path is being searched for
+var find_path = false // If the path is being searched for
 var path_found = false // If the previous searched path is found
 
 
 // Check if run button can be used
 function run_button_activation(){
-    const button = document.getElementById("run-button"); // Button element
-
-    const finding_path_color = {off: "bg-green", on: "bg-danger"} // Class names
-
-    // If the path is being searched for
-    if(find_path){
-        button.classList.replace(finding_path_color.off, finding_path_color.on) //  Off color for the button
-        
-        button.disabled = true
-        find_path = false 
-
-        // If the previous searched path is found
+   // If the path must be made
+    if(!find_path){
         if(path_found){
             path_found = false
-            clear_specific_nodes("path") // Clear every node that got an path background
+            clear_specific_nodes("path");
         }
+
+        find_path = true 
+        button.disabled = true
     }
     
     // If the path is found
     else{
-        button.classList.replace(finding_path_color.on, finding_path_color.off) // On color for the button
-        
+        find_path = false
         button.disabled = false
-        find_path = true
-        path_found = true
+        path_found = true  
     }
 }
