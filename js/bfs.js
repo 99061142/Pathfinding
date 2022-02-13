fc7395d377932f7c37e0c5ffc2e466e10f7e88ab
99061142 <99061142@mydavinci.nl>
2022-02-13 21:05:31 +0100

    Added the code to show the next node that gets searched on the board

--- js/bfs.js
+++ js/bfs.js
@@ -11,17 +11,28 @@ async function path_bfs(board, start, end){
             const col_num = col + direction_col; // Column of the node
 
             // If the node is the end position
-            if(row_num == end[0] && col_num == end[1]){
+            if([row_num, col_num].toString() === end.toString()){
                 return
             }
 
             // If the block is an empty node
             if(board[row_num] && board[row_num][col_num] === 0){
-                found_node(row_num, col_num); // Change the backgroundcolor
+                found_node(row_num, col_num); // Change the background color
 
                 positions.push([row_num, col_num]); // Let the node be checked the next round
                 
                 board[row_num][col_num] = 1; // Change the node to the wall value
+
+
+                for(const [direction_row, direction_col] of directions){
+                    const next_row_num = row_num + direction_row // Next row of the node
+                    const next_col_num = col_num + direction_col // Next column of the node
+
+                    // If there is a empty node next to the checked node
+                    if(board[next_row_num] && board[next_row_num][next_col_num] === 0){
+                        next_node(next_row_num, next_col_num); // Change the background color
+                    }
+                }
 
                 if(speed > 0){
                     await sleep(speed); // Wait an x amount of milliseconds
