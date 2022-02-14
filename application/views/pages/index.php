15255b7f74f0082c2a2f676d8318911d9a97a83f
99061142 <99061142@mydavinci.nl>
2022-02-14 19:47:58 +0100

    Changed the board to test the dfs functionallity

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -33,9 +33,9 @@
 <p class="text-center">* Information about the algorithm *</p>
 
 <div class="my-2 d-table" id="nodes" style="margin: auto;">
-    <?php for($row = 0; $row < 40; $row++){ ?>
+    <?php for($row = 0; $row < 15; $row++){ ?>
         <div>
-            <?php for($i = 0; $i < 40; $i++){ ?>
+            <?php for($i = 0; $i < 15; $i++){ ?>
                 <div class="node border border-dark float-left"></div>
             <?php } ?>
         </div>
