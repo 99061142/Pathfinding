04453a8bec15487ea7ecf4a729148d3711a9c604
99061142 <99061142@mydavinci.nl>
2022-04-09 19:04:48 +0200

    Changed the code a little to show the fastest path

--- js/bfs.js
+++ js/bfs.js
@@ -1,31 +1,35 @@
 // Check each node to until the ending node is found
 async function path_bfs(board, start, end){
-    const positions = [start]; // Positions of the node(s)
+    const positions = [[start]]; // Positions of the node(s)
     const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // Up, right, down and left
 
-    // Loop through every position
-    for(const [row, col] of positions){
-        // Check every direction
-        for(const [direction_row, direction_col] of directions){
+    // Loop through every list to go to the new node
+    for(let list of positions){
+        let [row, col] = list.slice(-1)[0] // Last node inside the list to go to the next node
+
+        // For every direction the node can go to
+        for(let [direction_row, direction_col] of directions){
             const row_num = row + direction_row; // Row of the node
             const col_num = col + direction_col; // Column of the node
 
             // If the node is the end position
             if([row_num, col_num].toString() == end.toString()){
+                show_end_path(list) // Show the end path from the start to the end node
                 return
             }
-
-            
 
             // If the block is an empty node
             if(board[row_num] && board[row_num][col_num] == 0){
                 found_node(row_num, col_num); // Change the background color
 
-                positions.push([row_num, col_num]); // Let the node be checked the next round
+                // Add the found node to the list with the previous nodes to go further with the found node when the loop gets called again 
+                new_list = [...list]
+                new_list.push([row_num, col_num])
+                positions.push(new_list);
                 
                 board[row_num][col_num] = 1; // Change the node to the wall value
 
-
+                // Change the nodes next to the found node to the next node color (if the node is empty)
                 for(const [direction_row, direction_col] of directions){
                     const next_row_num = row_num + direction_row // Next row of the node
                     const next_col_num = col_num + direction_col // Next column of the node
@@ -36,6 +40,7 @@ async function path_bfs(board, start, end){
                     }
                 }
 
+                // If the user wants to let the pathfinding algorithm go faster
                 if(speed){
                     await sleep(speed); // Wait an x amount of milliseconds
                 }
