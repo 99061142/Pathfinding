17d7733dda4f87414e8a98c984d228d1a553ddaa
99061142 <99061142@mydavinci.nl>
2022-04-16 20:10:51 +0200

    Deleted a closing div in the footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,5 @@
-        </div>
-        <footer class="bg-dark">
-            <p class="text-center text-white m-0 p-2"><?= date("Y"); ?></p>
+        <footer class="mt-4 bg-dark">
+            <p class="text-center text-white m-0 py-2"><?= date("Y"); ?></p>
         </footer>
     </body>
 </html>
