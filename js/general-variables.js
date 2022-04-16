77078f35277fa8f897296ff1ea16409bee039eb4
99061142 <99061142@mydavinci.nl>
2022-04-16 21:54:34 +0200

    General variables that gets used in multiple files

---
+++ js/general-variables.js
@@ -0,0 +1,4 @@
+const speeds = {slow: 100, normal: 50, fast: 20, instant: 0}
+var speed = speeds.normal; // Millisecond wait time the user chose
+
+const run_button = document.getElementById("run-button")
