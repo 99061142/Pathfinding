8a1bbd236eb7ed291d6bcff085085634525a298b
99061142 <99061142@mydavinci.nl>
2022-04-16 23:26:29 +0200

    Called all the js files

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,18 +15,10 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"> <!-- Font awesome -->
         <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>css/styling.css">
 
-        <script src="<?= base_url(); ?>js/general-variables.js" defer></script>
-        <script src="<?= base_url(); ?>js/general-pathfinding.js" defer></script>
-        <script src="<?= base_url(); ?>js/navigation-calls.js" defer></script>
-        <script src="<?= base_url(); ?>js/nodes.js" defer></script>
-
-
-        <!--<script src="<?= base_url(); ?>js/starting-functionality.js" defer></script> <!-- Add the attributes / onclicks to let the page work -->
-        <!--<script src="<?= base_url(); ?>js/board-styling.js"></script> <!-- Functions to change the styling for the board -->
-        <!-- <script src="<?= base_url(); ?>js/call-pathfinding.js"></script> <!-- Functions to get the path  -->
-        
-        
-
+        <script src="<?= base_url(); ?>js/general-variables.js" defer></script> <!-- General variables that gets used in multiple files -->
+        <script src="<?= base_url(); ?>js/general-pathfinding.js" defer></script> <!-- General functions for all the pathfinding calls -->
+        <script src="<?= base_url(); ?>js/navigation-calls.js" defer></script> <!-- Functionallity for the buttons inside the navigation -->
+        <script src="<?= base_url(); ?>js/nodes.js" defer></script> <!-- Functionallity for the board -->
 
         <!-- Pathfinding algorithm codes -->
         <script src="<?= base_url(); ?>js/bfs.js"></script>
