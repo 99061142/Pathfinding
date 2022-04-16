ca13e21487bc63241173fc4874b9bc747e7b072d
99061142 <99061142@mydavinci.nl>
2022-04-16 22:14:37 +0200

    Added the important position information to the general js file

--- js/general-variables.js
+++ js/general-variables.js
@@ -2,3 +2,18 @@ const speeds = {slow: 100, normal: 50, fast: 20, instant: 0}
 var speed = speeds.normal; // Millisecond wait time the user chose
 
 const run_button = document.getElementById("run-button")
+
+// Show if the important positions that can be on the board are placed (1 max on the board), and if both are used
+const important_position_information = {
+    start: {
+        row: null,
+        col: null,
+        used: false
+    },
+    
+    end: {
+        row: null,
+        col: null,
+        used: false
+    }
+}
