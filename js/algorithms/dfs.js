67852067e0ec8090f6e1d1cd0a3ec0ac8e252d56
99061142 <99061142@mydavinci.nl>
2022-05-16 23:21:29 +0200

    Save visited positions

--- js/algorithms/dfs.js
+++ js/algorithms/dfs.js
@@ -3,6 +3,7 @@ class Dfs {
     
     constructor() {
         this.queue = [BOARD.startPosition];
+        this.visited = [BOARD.startPosition.toString()];
         this.path = [];
     }    
 
@@ -11,32 +12,34 @@ class Dfs {
     }
 
     async run() {
+        // If queue isn't empty
         while(this.queue && this.queue.length) {
-            const [ROW, COL] = this.queue.shift(); // Position to move from + dequeue the position
+            const [ROW, COL] = this.queue.shift(); // Position to move from
 
-            // For every neighbour
             for(let directionPosition of this.#directions) {
                 const [DIRECTION_ROW, DIRECTION_COL] = directionPosition;
                 const NEXT_ROW = ROW + DIRECTION_ROW;
                 const NEXT_COL = COL + DIRECTION_COL;
                 const POSITION = [NEXT_ROW, NEXT_COL];
 
-                // If neighbour is empty, and not visited
-                if(BOARD.empty(POSITION) && BOARD.element(POSITION).id !== "next") {
+                // If neighbour is empty and not visited
+                if(BOARD.empty(POSITION) && !this.visited.includes(POSITION.toString())) {            
                     if(!BOARD.isEndPosition(POSITION)) {
-                        BOARD.next(POSITION); // Node styling to "next"
+                        BOARD.next(POSITION); // Update node styling
                         await BOARD.sleep();        
                     }
                     this.queue.unshift(POSITION); // Add neighbour to the queue
+                    this.visited.push(POSITION.toString()) // Set position as visited
                 }
             }
-            if(this.head) {
-                // If position is end position
+            
+            // If queue isn't empty
+            if(this.queue && this.queue.length) {
                 if(BOARD.isEndPosition(this.head)) {
-                    return this.path; // Return start -> end route
+                    return this.path; // Return start to end route
                 }
 
-                BOARD.found(this.head); // Node styling to "found"
+                BOARD.found(this.head); // Update node styling
             }
         }
     }
@@ -44,5 +47,5 @@ class Dfs {
 
 
 async function pathDfs() {
-    return new Dfs().run(); // Return the start -> end path
+    return new Dfs().run(); // Return the start to end path
 }
