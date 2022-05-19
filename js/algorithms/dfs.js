dae9435568873a1b86e1bbbf8702da7587862307
99061142 <99061142@mydavinci.nl>
2022-05-19 02:59:20 +0200

    Made the code more readable

--- js/algorithms/dfs.js
+++ js/algorithms/dfs.js
@@ -3,18 +3,28 @@ class Dfs {
     
     constructor() {
         this.queue = [BOARD.startPosition];
-        this.visited = [BOARD.startPosition.toString()];
+        this.visited = [BOARD.startPosition];
         this.path = [];
     }    
 
     get head() {
-        return this.queue[0]; // Position to move from
+        return this.queue[this.queue.length - 1]; // Last added position
+    }
+
+    positionVisited(position) {
+        // Returns if the position the path is on was already visited
+        for(let visitedPosition of this.visited) {
+            if(visitedPosition.toString() === position.toString()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     async run() {
         // If queue isn't empty
         while(this.queue && this.queue.length) {
-            const [ROW, COL] = this.queue.shift(); // Position to move from
+            const [ROW, COL] = this.queue.pop(); // Position to move from
 
             for(let directionPosition of this.#directions) {
                 const [DIRECTION_ROW, DIRECTION_COL] = directionPosition;
@@ -23,24 +33,22 @@ class Dfs {
                 const POSITION = [NEXT_ROW, NEXT_COL];
 
                 // If neighbour is empty and not visited
-                if(BOARD.empty(POSITION) && !this.visited.includes(POSITION.toString())) {            
+                if(BOARD.empty(POSITION) && !this.positionVisited(POSITION)) {            
                     if(!BOARD.isEndPosition(POSITION)) {
-                        BOARD.next(POSITION); // Update node styling
+                        BOARD.next(POSITION);
                         await BOARD.sleep();        
                     }
-                    this.queue.unshift(POSITION); // Add neighbour to the queue
-                    this.visited.push(POSITION.toString()) // Set position as visited
+                    this.queue.push(POSITION);
+                    this.visited.push(POSITION)
                 }
             }
-            
-            // If queue isn't empty
-            if(this.queue && this.queue.length) {
-                if(BOARD.isEndPosition(this.head)) {
-                    return this.path; // Return start to end route
-                }
+            if(!this.head) { return } // Path couldn't go further
 
-                BOARD.found(this.head); // Update node styling
+            // Return the path if the end position was found on the position the path is on
+            if(BOARD.isEndPosition(this.head)) {
+                return this.path;
             }
+            BOARD.found(this.head);
         }
     }
 }
