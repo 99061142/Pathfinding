f700a6f798578a3003f532c978edba5e7b97c7ea
99061142 <99061142@mydavinci.nl>
2022-05-23 18:16:19 +0200

    Function to get the fastest route
    Shows the fastest route from the start to end

--- js/algorithms/bfs.js
+++ js/algorithms/bfs.js
@@ -4,7 +4,7 @@ class Bfs {
     constructor() {
         this.queue = [BOARD.startPosition]; // Positions to move from
         this.visited = [BOARD.startPosition]; // ALl the positions that are visited
-        this.path = []; // Route from start to end position
+        this.path = {}; // Position of a node and the parent of it
     }    
 
     get head() {
@@ -14,9 +14,28 @@ class Bfs {
     positionVisited(position) {
         // Returns if the position was already visited
         for(let visitedPosition of this.visited) {
-            if(String(visitedPosition) === String(position)) { return true; }
+            if(String(position) === String(visitedPosition)) { return true; }
         }
         return false;
+    }
+
+    fastestPath() {
+        const ROUTE = []; // Route from start to end
+        var position = [];
+        var parent = BOARD.endPosition;
+
+        // If the current or parent position isn't the end position 
+        while(String(parent) !== String(BOARD.startPosition) && String(position) !== String(BOARD.startPosition)) {
+            position = this.path[parent];
+            parent = this.path[position];
+
+            // Add the positions to the route
+            ROUTE.push(position);
+            ROUTE.push(parent);
+        }
+
+        ROUTE.pop(); // Delete the start position
+        return ROUTE; // Return the fastest route
     }
 
     async run() {
@@ -39,10 +58,12 @@ class Bfs {
                     }
                     this.queue.push(POSITION);
                     this.visited.push(POSITION);
-                }
-            }
+
+                    this.path[String(POSITION)] = [ROW, COL]; // Add the position and the parent of the position
+                }    
+            }        
             if(!this.head){ return; } // Path couldn't go further
-            if(BOARD.isEndPosition(this.head)) { return this.path; } // End position is found
+            if(BOARD.isEndPosition(this.head)) { return this.fastestPath(); } // End position is found
             
             BOARD.found(this.head);
         }
