f21f82e5c450c85dfd5fa805463367a409b02efa
99061142 <99061142@mydavinci.nl>
2022-05-28 23:51:31 +0200

    Show the speed on the nav what the user chose
    Show the current speedType inside the navigation bar.
    
    (This is a temporary solution. The code gets changed later.)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const RUN_BUTTON = document.getElementById("run-button");
+const SPEED_NAV = document.getElementById("speed-nav");
 
 function capitalize(word) {
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
@@ -20,7 +21,17 @@ document.getElementById("algorithm-dropdown").querySelectorAll("button").forEach
 document.getElementById("speed-dropdown").querySelectorAll("button").forEach(speedButton => {
     // If the user clicks on an speed option
     speedButton.onclick = () => {
-        BOARD.speedTime = speedButton.innerText.toLowerCase(); // Update the speed value
+        const SPEED_TYPE = speedButton.innerText;
+        BOARD.speedTime = SPEED_TYPE.toLowerCase(); // Update the speed value
+
+        for(let speedType in BOARD.speedTypes) {
+            speedType = capitalize(speedType);
+    
+            if(SPEED_NAV.innerText.includes(speedType)) { 
+                SPEED_NAV.innerText = SPEED_NAV.innerText.replace(speedType, SPEED_TYPE); 
+                break;
+            }
+        }
     }
 })
 
