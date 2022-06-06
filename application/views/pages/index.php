8e90c661a522f7fa508a11d2cbe4b73e708d0090
99061142 <99061142@mydavinci.nl>
2022-06-06 02:44:14 +0200

    Added standard node classes inside the page itself

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -12,22 +12,22 @@
             </div>
 
             <div class="col-sm">    
-                <div id="wall" class="border border-dark d-inline-block p-2"></div>
+                <div class="border border-dark d-inline-block p-2" id="wall"></div>
                 <p class="d-inline-block">Walls</p>
             </div>
 
             <div class="col-sm">    
-                <div id="found" class="border border-dark d-inline-block p-2"></div>
+                <div class="border border-dark d-inline-block p-2" id="found"></div>
                 <p class="d-inline-block">Checked node</p>
             </div>
             
             <div class="col-sm">    
-                <div id="next" class="border border-dark d-inline-block p-2"></div>
+                <div class="border border-dark d-inline-block p-2" id="next"></div>
                 <p class="d-inline-block">Next node</p>
             </div>
 
             <div class="col-sm">    
-                <div id="fastest" class="border border-dark d-inline-block p-2"></div>
+                <div class="border border-dark d-inline-block p-2" id="fastest"></div>
                 <p class="d-inline-block">Fastest route</p>
             </div>
         </div>
@@ -38,7 +38,7 @@
             <?php for($row = 0; $row <= 36; $row++){ ?>
                 <tr id="row">
                     <?php for($col = 0; $col <= 36; $col++){ ?>
-                        <td class="node"></td>
+                        <td class="node border border-dark float-left"></td>
                     <?php } ?>
                 </tr>
             <?php } ?>
