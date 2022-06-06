deb31ca171ec24c1f060dc0f82510e034f367547
99061142 <99061142@mydavinci.nl>
2022-06-06 02:47:42 +0200

    Class to change a single node

---
+++ js/node.js
@@ -0,0 +1,113 @@
+export class Node {
+    element(position) {
+        const [ROW, COL] = position;
+        const ROW_ELEMENT = document.getElementById('board').children[ROW];
+        
+        if(ROW_ELEMENT) { return ROW_ELEMENT.children[COL]; } // Return the element if it exists
+    }
+
+    position(element) {
+        const ROW_ELEMENT = element.parentElement;
+        const ROW = Array.from(document.getElementById('board').children).indexOf(ROW_ELEMENT);
+        const COL = Array.from(ROW_ELEMENT.children).indexOf(element);
+
+        return [ROW, COL]; // Return the position of the element
+    }
+
+    empty(position) {
+        const [ROW, COL] = position;
+
+        return this.list[ROW] && this.list[ROW][COL] === 0; // Return if the position is empty on the board
+    }
+
+    isStartPosition(position) {
+        return String(position) === String(this.startPosition);
+    }
+
+    get startElement() {
+        return document.getElementById("start");
+    }
+    
+    get startPosition() {
+        if(this.startElement) { return this.position(this.startElement); }
+    }
+
+    set startPosition(element) {
+        // Change the element to the start node
+        element.id = "start";
+        element.className += " fas fa-arrow-right bg-success";
+
+        if(this.endPosition) { this.runButton.disabled = false; }
+    }
+
+    isEndPosition(position) {
+        return String(position) === String(this.endPosition);
+    }
+
+    get endElement() {
+        return document.getElementById("end");
+    }
+
+    get endPosition() {
+        if(this.endElement) { return this.position(this.endElement); }
+    }
+
+    set endPosition(element) {
+        // Change the element to the end node
+        element.id = "end";
+        element.className += " fas fa-home bg-danger";
+
+        if(this.startPosition) { this.runButton.disabled = false; }
+    }
+
+    setStandardAttributes(element) {
+        // Disable the button when the start or end position is not on the board
+        if(element === this.startElement || element === this.endElement){ this.runButton.disabled = true; }
+
+        // set the position on the board to empty
+        const POSITION = this.position(element);
+        this.emptyBoardColumn(POSITION);
+
+        // set the element to the standard attributes
+        element.className = this.standardClasses;
+        element.id = "";
+    }
+
+    wall(position) {  
+        const ELEMENT = this.element(position);
+
+        // If the node is not important
+        if(!this.importantNames.includes(ELEMENT.id)) {
+            // Change the element to a wall
+            ELEMENT.className = this.standardClasses;
+            ELEMENT.id = "wall";
+
+            this.fillBoardColumn(position); // set the position on the board to filled
+        }
+    }
+
+    found(position) {
+        // Change the element to a found node
+        const ELEMENT = this.element(position); 
+        ELEMENT.className = this.standardClasses;
+        ELEMENT.id = "found";
+    }
+
+    async next(position) {    
+        // Change the element to a next node
+        const ELEMENT = this.element(position);
+        ELEMENT.className = this.standardClasses;
+        ELEMENT.id = "next";
+
+        await this.sleep(); // wait for the animation to finish
+    }
+
+    async fastest(position) {  
+        // Change the element to a fastest node
+        const ELEMENT = this.element(position);
+        ELEMENT.className = this.standardClasses;
+        ELEMENT.id = "fastest";
+
+        await this.sleep(); // wait for the animation to finish
+    }
+}
