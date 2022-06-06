36dcbcd0528708c4f3f45a5fb29f7be7e65c5a5b
99061142 <99061142@mydavinci.nl>
2022-06-06 02:50:14 +0200

    Class to add onclicks / btn text changes for nav
    Add onclick events for the button inside the navigation bar, and change some buttons text when something gets changed (ex: change speed name at the dropdown button when the speed gets changed).

---
+++ js/navigation.js
@@ -0,0 +1,84 @@
+import { Board } from "./board.js";
+
+export class Navigation extends Board {
+    constructor() {
+        super();
+        this.speedDropdown = document.getElementById("speed-dropdown");
+        this.runButton = document.getElementById("run-button");
+        
+        // Attach the event listeners for every button on the nav bar
+        this.algorithmOptionsOnclick();
+        this.speedOptionsOnclick();
+        this.runButtonOnclick();
+        this.clearBoardOnclick();
+        this.clearWallsOnclick();
+        this.clearAlgorithmPathOnclick();
+        this.randomWallsOnclick();
+
+        // Change the text of the run/speed button to its default value
+        this.runButtonText();
+        this.speedOptionsText();
+    }
+
+    runButtonText(algorithm=null) {
+        // If the algorithm is not specified, then it is the first time the function is called
+        if(!algorithm) { 
+            this.runButton.innerHTML += this.runButton.value.toUpperCase(); 
+        }
+        else {
+            // Replace the current algorithm with the new one
+            this.runButton.innerText = this.runButton.innerText.replace(this.runButton.value.toUpperCase(), algorithm);
+        }
+    }
+
+    speedOptionsText(speedType=null) {
+        // If the speed type is not specified, then it is the first time the function is called
+        if(!speedType) { 
+            this.speedDropdown.innerText += capitalize(this.currentSpeedType);
+        }
+        else {
+            // Replace the current speed type with the new one
+            this.speedDropdown.innerText = this.speedDropdown.innerText.replace(capitalize(this.currentSpeedType), speedType);
+        }
+    }
+
+    algorithmOptionsOnclick() {
+        // Attach the onclick event for each algorithm option
+        document.getElementById("algorithm-options").querySelectorAll("button").forEach(algorithmButton => {
+            algorithmButton.onclick = () => {
+                this.runButtonText(algorithmButton.innerText);
+                this.runButton.value = algorithmButton.innerText.toLowerCase();
+            }
+        });
+    }
+
+    speedOptionsOnclick() {
+        // Attach the onclick event for each speed option
+        document.getElementById("speed-options").querySelectorAll("button").forEach(speedButton => {
+            speedButton.onclick = () => {
+                this.speedOptionsText(speedButton.innerText);
+                this.currentSpeedType = speedButton.innerText.toLowerCase();
+            }
+        });
+    }
+
+    runButtonOnclick() {
+        this.runButton.onclick = () => this.makePath(this.runButton.value); // Run the algorithm
+    }
+
+    clearBoardOnclick() {
+        document.getElementById("clear-board").onclick = () => this.clearBoard();
+    }
+
+    clearWallsOnclick() {
+        document.getElementById("clear-walls").onclick = () => this.clearWalls();
+    }
+
+    clearAlgorithmPathOnclick() {
+        document.getElementById("clear-path").onclick = () => this.clearAlgorithmPath();
+    }
+
+    randomWallsOnclick() {
+        document.getElementById("random-walls").onclick = () => this.randomWalls(); // Create random walls
+    }
+}
