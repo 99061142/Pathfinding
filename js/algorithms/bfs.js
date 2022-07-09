58303e9e141c727a2979fd37be6e3d58022c2134
99061142 <99061142@mydavinci.nl>
2022-07-09 13:51:22 +0200

    Made the code faster + decreased lines of code

--- js/algorithms/bfs.js
+++ js/algorithms/bfs.js
@@ -2,9 +2,9 @@ export class Bfs {
     #directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // Up, right, down and left
     
     constructor(board) {
+        this.board = board;
         this.queue = [board.startPosition]; // Positions to move from
         this.path = {}; // Position of a node and the parent of it
-        this.board = board;
     }    
 
     get head() {
@@ -20,42 +20,33 @@ export class Bfs {
     }
 
     visited(position) {
-        return String(position) !== String(this.board.startPosition) && !this.path.hasOwnProperty(String(position));
+        return String(position) === String(this.board.startPosition) || this.path.hasOwnProperty(String(position));
     }
 
     fastestPath() {
         const PATH = [];
         let parent = this.board.endPosition;
 
-        // Get the fastest path from the end to start position
-        while(String(PATH[PATH.length - 1]) !== String(this.board.startPosition)) {    
-            const POSITION = this.path[parent]; // Current position
-            parent = this.path[POSITION]; // Parent position
-
-            if(POSITION){ PATH.push(POSITION); }
-            if(parent){ PATH.push(parent); }
+        while(parent) {
+            PATH.push(parent);
+            parent = this.path[String(parent)];
         }
-        return PATH.slice(0, -1); // Return the fastest path
+        return PATH.slice(1, -1);
     }
 
     async run() {
-        // If queue isn't empty
         while(this.queue && this.queue.length) {
-            const [ROW, COL] = this.dequeue(); // Position to move from
-
-            // For every optional direction
-            for(let [directionRow, directionCol] of this.#directions) {
-                const NEXT_ROW = ROW + directionRow;
-                const NEXT_COL = COL + directionCol;
-                const POSITION = [NEXT_ROW, NEXT_COL];
+            for(let direction of this.#directions) {
+                const NEIGHBOUR_POSITION = neighbourPosition(this.head, direction);
 
                 // If neighbour is empty and not visited
-                if(this.board.empty(POSITION) && this.visited(POSITION)) {    
-                    if(!this.board.isEndPosition(POSITION)) { await this.board.next(POSITION); }
-                    this.path[String(POSITION)] = [ROW, COL]; // Add the position and the parent of the position
-                    this.enqueue(POSITION);
+                if(this.board.empty(NEIGHBOUR_POSITION) && !this.visited(NEIGHBOUR_POSITION)) {    
+                    if(!this.board.isEndPosition(NEIGHBOUR_POSITION)) { await this.board.next(NEIGHBOUR_POSITION); }
+                    this.path[String(NEIGHBOUR_POSITION)] = this.head; // Add the position and the parent of the position
+                    this.enqueue(NEIGHBOUR_POSITION);
                 }    
             }    
+            this.dequeue(); // Remove head from queue
             if(!this.head){ return; } // Path couldn't go further
             if(this.board.isEndPosition(this.head)) { return this.fastestPath(); }
             this.board.found(this.head);
