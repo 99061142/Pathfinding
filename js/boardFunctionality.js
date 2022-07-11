fd2a8775a962a1b33e460d3f59207d0fccad7f89
99061142 <99061142@mydavinci.nl>
2022-07-11 03:06:46 +0200

    Made it more easier to add weight nodes
    Wall: left click / hover + left click.
    Weight: middle click / hover + middle click.

--- js/boardFunctionality.js
+++ js/boardFunctionality.js
@@ -3,43 +3,74 @@ import { Navigation } from "./navigation.js";
 class BoardFunctionality extends Navigation {
     constructor() {
         super();
-        this.addNodelisteners();
+        this.addEventlisteners();
     }
 
-    addNodelisteners() {
+    addEventlisteners() {
         this.nodes.forEach(nodeElement => {
-            this.nodeOnclick(nodeElement);
             this.nodeMouseover(nodeElement);
+            this.nodeOnclick(nodeElement)
         });
     }
 
-    nodeOnclick(nodeElement) {
-        nodeElement.onclick = () => { 
-            if(!this.isRunning) {
-                if(!this.startElement && nodeElement != this.endElement){
-                    this.startPosition = nodeElement
-                }
-                else if(!this.endElement && nodeElement != this.startElement) {
-                    this.endPosition = nodeElement
-                }else if(nodeElement.id && nodeElement.id != 'wall') {
-                    this.setStandardAttributes(nodeElement)
-                }else if(nodeElement.id == 'wall') {
-                    const POSITION = this.position(nodeElement);
-                    this.weight(POSITION)
-                }else {
-                    const POSITION = this.position(nodeElement);
-                    this.wall(POSITION);
-                }
-            }
+    mouseoverLeftClick(nodeElement) {
+        const POSITION = this.position(nodeElement);
+        this.wall(POSITION);
+    }
+
+    mouseoverMiddleClick(nodeElement) {
+        if(nodeElement.id != 'wall') {
+            const POSITION = this.position(nodeElement);
+            this.weight(POSITION);
         }
     }
 
     nodeMouseover(nodeElement) {
         nodeElement.addEventListener("mouseover", mouseEvent => {
-            if(!this.isRunning && mouseEvent.buttons === 1) { 
-                const POSITION = this.position(nodeElement);
-                if(nodeElement.id == "wall") { this.weight(POSITION); }
-                else { this.wall(POSITION); }
+            if(!this.isRunning) {
+                switch(mouseEvent.buttons){
+                    case 1:
+                        this.mouseoverLeftClick(nodeElement);
+                        break;
+                    case 4:
+                        this.mouseoverMiddleClick(nodeElement);
+                        break;
+                }
+            }
+        });
+    }
+
+    clickLeftClick(nodeElement) {
+        if(!this.startElement && nodeElement != this.endElement){
+            this.startPosition = nodeElement
+        }
+        else if(!this.endElement && nodeElement != this.startElement) {
+            this.endPosition = nodeElement
+        }else if(nodeElement.id) {
+            this.setStandardAttributes(nodeElement)
+        }else {
+            const POSITION = this.position(nodeElement);
+            this.wall(POSITION);
+        }
+    }
+
+    clickMiddleClick(nodeElement) {
+        const POSITION = this.position(nodeElement);
+        this.weight(POSITION);
+    }
+
+    nodeOnclick(nodeElement) {
+        // when the user presses the middle mouse button while hovering over a node,
+        nodeElement.addEventListener("mousedown", mouseEvent => {
+            if(!this.isRunning) {
+                switch(mouseEvent.buttons){
+                    case 1:
+                        this.clickLeftClick(nodeElement);
+                        break;
+                    case 4:
+                        this.clickMiddleClick(nodeElement);
+                        break;
+                }
             }
         });
     }
