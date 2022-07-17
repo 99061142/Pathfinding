79e85be0029212c11c2f3170467d5260bae41ba6
99061142 <99061142@mydavinci.nl>
2022-07-17 04:55:44 +0200

    Include weights and find start to end path
    Have included the possibility for weights, and the path can now be found.

--- js/algorithms/astar.js
+++ js/algorithms/astar.js
@@ -33,14 +33,15 @@ export class Astar {
         let [startRow, startCol] = this.board.startPosition;
         let rowDif = Math.abs(startRow - row);
         let colDif = Math.abs(startCol - col);
-        let distance = 0;
+        let distance = Number(this.board.element(position).dataset.distance) / 10;
 
         while(rowDif || colDif) {
             distance += (rowDif && colDif) ? 1.4 : 1;
             if(rowDif) { rowDif -= 1; }
             if(colDif) { colDif -= 1; }
         }        
-        return Math.round(distance * 100) / 10; // 1 decimal
+        distance = Math.round(distance * 100) / 10; // 1 decimal
+        return distance;
     }
 
     toEndDistance(position) {
@@ -59,7 +60,7 @@ export class Astar {
         return Math.round(distance * 100) / 10; // 1 decimal
     }
 
-    fCost(position) {
+    totalDistance(position) {
         return this.toStartDistance(position) + this.toEndDistance(position); // Distance to start + distance to end
     }
 
@@ -69,7 +70,7 @@ export class Astar {
         /* Get the position with the lowest distance cost or 
         get the position with the lowest toEndDistance if there are multiple positions with the same distance cost */
         this.queue.forEach(position => {
-            let positionFCost = this.fCost(position);
+            let positionFCost = this.totalDistance(position);
             let endCost = this.toEndDistance(position);
             
             if(!dict || positionFCost < dict.distance || positionFCost == dict.distance && endCost < dict.toEndDistance) {
@@ -93,8 +94,8 @@ export class Astar {
     }
 
     addPathInformation(position, parentPosition) {
-        let pathPosition = this.path[String(position)];
-        pathPosition.distance = this.fCost(position);
+        let pathPosition = this.pathPosition(position);
+        pathPosition.distance = this.totalDistance(position);
         pathPosition.parent = String(parentPosition);
     }
 
@@ -118,22 +119,40 @@ export class Astar {
         return path.slice(1, -1);
     }
 
+    pathPosition(position) {
+        return this.path[String(position)];
+    }
+
     getClosestNeighbour(position) {
-        let pathPosition = this.path[String(position)];
+        let pathPosition = this.pathPosition(position);
 
         for(let direction of this.#directions) {
             let neighbour = neighbourPosition(position, direction)
 
-            // If the position is visited
+            // If the neighbour position is visited
             if(this.board.empty(neighbour) && this.board.element(neighbour) && this.isVisited(neighbour)) {
                 let neighbourDistance = this.path[String(neighbour)].distance;
+                let parentPositionList = pathPosition.parent.split(',').map(Number);
 
-                // If the neighbour fCost is lower than the current position neighbour fCost
-                if(neighbourDistance != 0 && neighbourDistance < pathPosition.distance) {
-                    pathPosition.distance = neighbourDistance;
+                /* If the neighbour totalDistance is lower than the current position neighbour totalDistance or 
+                when the neighbour totalDistance is equal to the current position neighbour totalDistance and 
+                the neighbour position is closer to the start position */
+                if(neighbourDistance < pathPosition.distance || neighbourDistance == pathPosition.distance && this.toStartDistance(neighbour) < this.toStartDistance(parentPositionList)) {
+                    pathPosition.distance = (neighbourDistance != 0) ? neighbourDistance : this.totalDistance(neighbour);
                     pathPosition.parent = String(neighbour);
                 }
             }
+        }
+    }
+
+    positionFound(position) {
+        this.visited.push(position);
+        this.dequeue(position);
+        if(!this.board.isStartPosition(position)) { this.getClosestNeighbour(position); } // Select the closest neighbour as parent position
+
+        if(!this.board.isStartPosition(position) && !this.board.isEndPosition(position)) {
+            if(!this.board.element(position).id.includes("weight")) { this.board.found(position); }
+            else{ this.board.weightFound(position); }
         }
     }
 
@@ -142,7 +161,7 @@ export class Astar {
             let position = this.head;
 
             for(let direction of this.#directions) {
-                let neighbour = neighbourPosition(position, direction)
+                let neighbour = neighbourPosition(position, direction);
                 
                 // If neighbour is empty and not visited
                 if(this.board.element(neighbour) && this.board.empty(neighbour) && !this.isVisited(neighbour)) {
@@ -155,16 +174,8 @@ export class Astar {
                     }
                 }
             }
+            this.positionFound(position);
             if(this.isQueued(this.board.endPosition)) { return this.getFastestPath; }
-            
-            // Add position
-            this.visited.push(position);
-            if(!this.board.isStartPosition(position)) { 
-                this.board.found(position); 
-                this.getClosestNeighbour(position);
-            }
-
-            this.dequeue(position);
             await this.board.sleep();
         }
     }
