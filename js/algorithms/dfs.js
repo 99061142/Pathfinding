79455bd2749dd9090d8250841e893fafdcfbd0df
99061142 <99061142@mydavinci.nl>
2022-07-19 03:57:06 +0200

    Made code more readable

--- js/algorithms/dfs.js
+++ js/algorithms/dfs.js
@@ -2,14 +2,14 @@ export class Dfs {
     #directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // Left, down, right and up
     
     constructor(board) {
+        this.board = board;
         this.queue = [board.startPosition];
-        this.visited = [board.startPosition];
+        this.visited = [String(board.startPosition)];
         this.path = [];
-        this.board = board
     }    
 
     get head() {
-        return this.queue[this.queue.length - 1]; // Last added position
+        return this.queue.at(-1);
     }
 
     enqueue(position) {
@@ -20,24 +20,24 @@ export class Dfs {
         return this.queue.pop();
     }
 
+    isVisited(position) {
+        return this.visited.includes(String(position));
+    }
+
+    isQueued(position) {        
+        return this.queue.find(queuedPosition => String(queuedPosition) === String(position));
+    }
+
+    pathPositionIndex(position) {
+        return this.path.findIndex(pathPosition => String(pathPosition) === String(position));
+    }
+
     positionvisited(position) {
-        return this.visited.map(String).includes(String(position)) || this.queued(position);
+        return this.isVisited(position) || this.isQueued(position);
     }
 
-    positionIndexInPath(position) {
-        return this.path.map(String).indexOf(String(position));
-    }
-
-    queued(position) {        
-        return this.queue.map(String).includes(String(position));   
-    }
-
-    neighbourIsEnd(position) {
-        // Return if a neighbour is the end position
-        return this.#directions.some(direction => {
-            let neighbour = neighbourPosition(position, direction);
-            return this.board.isEndPosition(neighbour);
-        });
+    nextPathPositionIndex(position) {
+        return this.pathPositionIndex(position) + 1;
     }
 
     isNeighbour(positionOne, positionTwo){
@@ -48,68 +48,78 @@ export class Dfs {
         });
     }
 
-    canMove(position) {
-        // Check if the position can move to 1 of the possible directions
+    neighbourIsEnd(position) {
+        return this.#directions.some(direction => {
+            let neighbour = neighbourPosition(position, direction);
+            return this.board.isEndPosition(neighbour);
+        });
+    }
+
+    canMoveToAnyNeighbour(position) {
         return this.#directions.some(direction => {
             let neighbour = neighbourPosition(position, direction);
             return this.board.empty(neighbour) && !this.positionvisited(neighbour);
         });
     }
 
-    firstNeigbourOfEndPosition() {
+    get firstNeigbourOfEndPosition() {
         // Return the first neighbour of the end position
         return this.path.find(position => { return this.neighbourIsEnd(position); });
     }
 
     pathToEndPosition() {    
-        // Return the path to the end position
-        const NEIGHBOUR_OF_END_POSITION = this.firstNeigbourOfEndPosition();
-        this.path.splice(this.nextPositionIndex(NEIGHBOUR_OF_END_POSITION), this.path.length);
-    }
-
-    nextPositionIndex(position) {
-        return this.positionIndexInPath(position) + 1;
+        // Delete every path position in the list after the end position index
+        let neighbourOfEndPosition = this.firstNeigbourOfEndPosition;
+        let neighbourOfEndpositionIndex = this.nextPathPositionIndex(neighbourOfEndPosition);
+        this.path.splice(neighbourOfEndpositionIndex, this.path.length);
     }
 
     nextPosition(position) {
-        return this.path[this.nextPositionIndex(position)];
+        let nextPathPositionIndex = this.nextPathPositionIndex(position);
+        return this.path[nextPathPositionIndex];
     }
 
     deleteIncorrectPositions() {
         for(let position of this.path.reverse()) {
             // Delete every next position if it isn't a neighbour of the current position
-            while(this.nextPosition(position) && !this.isNeighbour(position, this.nextPosition(position))) {
-                this.path.splice(this.nextPositionIndex(position), 1);
+            while(this.nextPosition(position) && !this.isNeighbour(position, this.nextPosition(position))) {    
+                this.path.splice(this.nextPathPositionIndex(position), 1);
             } 
         }
     }
 
-    fastestPath() {
+    get fastestPath() {
         this.pathToEndPosition();
         this.deleteIncorrectPositions();
         return this.path; 
+    }
+
+    canMove(position) {
+        return this.board.empty(position) && !this.positionvisited(position);
+    }
+
+    setVisited(position) {
+        this.visited.push(String(position));
     }
 
     async run() {      
         while(this.queue && this.queue.length) {
             let position = this.dequeue();
 
-            // For every optional direction
             for(let direction of this.#directions) {
                 let neighbour = neighbourPosition(position, direction);
 
                 // If neighbour is empty and not visited
-                if(this.board.empty(neighbour) && !this.positionvisited(neighbour)) { 
+                if(this.canMove(neighbour)) { 
                     if(!this.board.isEndPosition(neighbour)) { await this.board.next(neighbour); }
                     this.enqueue(neighbour);
                 }
             }
+            if(this.board.isEndPosition(this.head)) { return this.fastestPath; }
             if(!this.head) { return; } // Path couldn't go further
-            if(this.board.isEndPosition(this.head)) { return this.fastestPath(); } // End position was found
-
+            if(this.canMoveToAnyNeighbour(this.head)) { this.path.push(this.head); } // If the position can move further
             this.board.found(this.head);
-            this.visited.push(this.head); // Position is visited
-            if(this.canMove(this.head)) { this.path.push(this.head); } // If the position can move further
+            this.setVisited(this.head);
             await this.board.sleep();
         }
     }
