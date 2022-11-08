b1c7d8b25f37ca878cd97245f8f0f2db7a4d3b48
99061142 <xarbprive@gmail.com>
2022-11-08 11:47:25 +0100

    Get dict of weights of every node

--- js/classes/board/board.js
+++ js/classes/board/board.js
@@ -4,10 +4,36 @@ export class Board {
     constructor(row, node) {
         this._board = document.getElementById("board");
         this._node = node;
+        this._nodes = {};
         this._row = row;
         this.nodeHeight = node.offsetHeight;
         this.nodeWidth = node.offsetWidth;
         this.init();
+    }
+
+    init() {
+        // Remove the node that was already created
+        this._node.remove();
+
+        this.addRows();
+        this.addNodes();
+    }
+
+    get dict() {
+        let dict = {};
+        let rows = this._board.children;
+
+        for(let [i, row] of Object.entries(rows)) {
+            dict[i] = [];
+
+            let nodes = row.children;
+            for(let node of nodes) {
+                let weight = node.dataset.weight;
+                weight = parseInt(weight);
+                dict[i].push(weight);
+            }
+        }
+        return dict;
     }
 
     get maxWidth() {
@@ -34,20 +60,22 @@ export class Board {
         return columnChange * 0.75;
     }
 
-    addNode(row) {
-        new Node(row, this._node);
+    addNode(row, rowIndex) {
+        let node = new Node(row, this._node);
+        this._nodes[rowIndex].push(node)
     }
 
     addNodes() {
         let columnChange = this.columnChange;
         let rows = this._board.children;
 
-        for(let row of rows) {
+        for(let [rowIndex, row] of Object.entries(rows)) {
             let currentColumns = row.children.length;
             let columnsAmount = columnChange - currentColumns;
 
+            this._nodes[rowIndex] = [];
             for(let i = 0; i < columnsAmount; i++) {
-                this.addNode(row);
+                this.addNode(row, rowIndex);
             }
         }
     }
@@ -62,14 +90,5 @@ export class Board {
         for(let i = 0; i < this.rowChange; i++) {
             this.addRow();
         }
-    }
-
-
-    init() {
-        // Remove the node that was already created
-        this._node.remove();
-
-        this.addRows();
-        this.addNodes();
     }
 }
