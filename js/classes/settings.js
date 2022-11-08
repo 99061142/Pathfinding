1e09dc9ab5c2e7562252df3f70177847bfe0319e
99061142 <xarbprive@gmail.com>
2022-11-08 11:45:28 +0100

    Add event listeners for start button

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -1,5 +1,12 @@
 import { Board } from "./board/board.js";
 
+// Initialize and create board
 let row = document.getElementById("exampleRow");
 let node = document.getElementById("exampleNode");
-new Board(row, node);
+let board = new Board(row, node);
+
+// Event listener for start button
+let startButton = document.getElementById("start");
+startButton.addEventListener("click", () => {
+
+});
