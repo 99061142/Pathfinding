1148b9e3ec976f6eac91a387344737ccd3de381e
99061142 <xarbprive@gmail.com>
2022-11-08 21:32:01 +0100

    Update settings state for start/end of running

--- js/classes/run.js
+++ js/classes/run.js
@@ -1,8 +1,9 @@
 import { Bfs } from './algorithms/bfs.js';
 
 export class Run {
-    constructor() {
+    constructor(switchSettingsState) {
         this._running = false;
+        this._switchSettingsState = switchSettingsState;
     }
 
     get algorithm() {
@@ -12,15 +13,27 @@ export class Run {
             case "bfs":
                 return Bfs;
             default:
+                this._running = false;
+                this._switchSettingsState(false);
                 throw new Error("Algorithm not recognized");
         }
     }
 
     async run(dict, nodes) {
-        if(this._running) { return; }
+        let start = document.getElementById("start");
+        let end = document.getElementById("end");
+
+        // If a algorithm is running or the start or end node is not placed, return
+        if(this._running || !start || !end) { 
+            return; 
+        }
+        this._running = true;
+        this._switchSettingsState(true);
 
         // Run the algorithm
         await new this.algorithm(dict, nodes).run().then(() => {
+            this._running = false;
+            this._switchSettingsState(false);
 
         });
 
