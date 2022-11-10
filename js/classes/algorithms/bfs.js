d1a7619e15c42691681f15878b66de8f7d8e16d5
99061142 <xarbprive@gmail.com>
2022-11-10 22:17:32 +0100

    Check each node around the start if it's movable

--- js/classes/algorithms/bfs.js
+++ js/classes/algorithms/bfs.js
@@ -3,8 +3,24 @@ import { Algorithm } from './algorithm.js';
 export class Bfs extends Algorithm {
     constructor(nodes) {
         super(nodes);
+        this._directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // right, down, left, up
     }
 
     async run() {
+        let start = this.startPosition;
+
+        while(true) {
+            let queue = [start];
+            let queuedPosition = queue.shift();
+
+            for(let direction of this._directions) {
+                let position = this.position(queuedPosition, direction);
+
+                if(this.isMovable(position)) {
+                    queue.push(position);
+                }
+            }
+            break;
+        }
     }
 }
