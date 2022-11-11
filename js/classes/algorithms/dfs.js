1212df3bca3cc513a8a0ed8b2b096bf959fc0b89
99061142 <xarbprive@gmail.com>
2022-11-11 21:45:18 +0100

    Run DFS algorithm until end is found or stuck

--- js/classes/algorithms/dfs.js
+++ js/classes/algorithms/dfs.js
@@ -3,10 +3,34 @@ import { Algorithm } from './algorithm.js';
 export class Dfs extends Algorithm {
     constructor(nodes) {
         super(nodes);
-        this._directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // right, down, left, up
+        this._directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // left bottom right up
+        this._start = this.startPosition;
     }
 
     async run() {
+        // Positions to check
+        let queue = [this._start];
 
+        while(queue && queue.length) {
+            let queuedPosition = queue.pop();
+
+            for(let direction of this._directions) {
+                let position = this.position(queuedPosition, direction);
+
+                // Skip if the position on the board is already checked or not empty
+                if(!this.isMovable(position)) { continue; }
+
+                // Push the position to the queue
+                queue.push(position);
+                this.node(position).next();
+            }
+            await this.node(queuedPosition).visited();
+
+            // If the end position is found, return the path
+            let nextQueuedPosition = queue[queue.length - 1];
+            if(queue.length && this.node(nextQueuedPosition).isEnd()) {
+                return
+            }
+        }
     }
 }
