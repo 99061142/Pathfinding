e751f3daeabc8f4eb729183a409ff2aa97e71a2c
99061142 <xarbprive@gmail.com>
2022-11-11 19:59:38 +0100

    Search board until end position is found
    Search the board until the end position is found, or until the queue is empty (this can happen when the end position can't be reached).

--- js/classes/algorithms/bfs.js
+++ js/classes/algorithms/bfs.js
@@ -4,23 +4,34 @@ export class Bfs extends Algorithm {
     constructor(nodes) {
         super(nodes);
         this._directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // right, down, left, up
+        this._start = this.startPosition;
     }
 
     async run() {
-        let start = this.startPosition;
+        // Positions to check
+        let queue = [this._start];
 
-        while(true) {
-            let queue = [start];
+        while(queue && queue.length) {
+            // Select the first position in the queue and remove it from the queue
             let queuedPosition = queue.shift();
 
             for(let direction of this._directions) {
+                // Get the neighbour position
                 let position = this.position(queuedPosition, direction);
 
-                if(this.isMovable(position)) {
-                    queue.push(position);
-                }
+                // Skip if the position on the board is already checked or not empty
+                if(!this.isMovable(position)) { continue; }
+
+                // Push the position to the queueS
+                queue.push(position);
+                await this.next(position);
             }
-            break;
+
+            // If the end position is found, return the path
+            if(queue.length && this.isEnd(queue.at(0))) { 
+                return
+            }
+            await this.visited(queuedPosition);
         }
     }
 }
