6418ed2ae3453e14fe67f1892668418af61f76be
99061142 <xarbprive@gmail.com>
2022-11-15 19:18:48 +0100

    Starting class for a*

---
+++ js/classes/algorithms/astar.js
@@ -0,0 +1,11 @@
+import { Algorithm } from './algorithm.js';
+
+export class Astar extends Algorithm {
+    constructor(nodes) {
+        super(nodes);
+    }
+
+    async run() {
+        
+    }
+}
