b2d0639f07adf50a94c75549ee3c74375e48c043
99061142 <xarbprive@gmail.com>
2022-11-15 19:08:29 +0100

    List with the unweighted algorithm names

--- js/classes/run.js
+++ js/classes/run.js
@@ -6,6 +6,7 @@ export class Run {
     constructor(switchSettingsState) {
         this.running = false;
         this._switchSettingsState = switchSettingsState;
+        this.unweightedAlgorithms = ["bfs", "dfs"];
     }
 
     get algorithmName() {
