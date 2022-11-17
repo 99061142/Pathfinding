26c44e7154f642c897322c03b6bb953c3e1fc08c
99061142 <xarbprive@gmail.com>
2022-11-17 21:15:28 +0100

    Functionality for new dropdown navs
    * Add onclick event to new algorithm / pencil dropdown menus.
    
    * Disable onclick on the options when the algorithm is unweighted, and the option is weighted.
    
    * When the user chooses an unweighted algorithm, delete all weighted nodes on the board.

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -1,11 +1,6 @@
 import { Board } from "./board/board.js";
 import { Run } from "./run.js";
 
-// Set the settings state based on if the algorithm is running
-function switchSettingsState(state) {
-    runButton.disabled = state;
-}
-
 // Initialize run class
 let run = new Run(switchSettingsState);
 
@@ -13,6 +8,33 @@ let run = new Run(switchSettingsState);
 let row = document.getElementById("exampleRow");
 let node = document.getElementById("exampleNode");
 let board = new Board(row, node);
+
+// Set the settings state based on if the algorithm is running
+function switchSettingsState(state) {
+    runButton.disabled = state;
+}
+
+// Enable or disable settings based on if the algorithm is weighted or unweighted
+let weightedOptionElements = document.getElementsByClassName("weightedAlgorithm")
+weightedOptionElements = Array.from(weightedOptionElements);
+function weightedOptionElementsState(state) {
+    // If the state is true (chosen algorithm is weighted), enable the weighted options
+    if(state) {
+        weightedOptionElements.forEach((option) => {
+            option.classList.remove("disabled");
+        });
+        return
+    }    
+
+    weightedOptionElements.forEach((option) => {
+        option.classList.add("disabled");
+    });
+}
+
+// set settings state based on the algorithm when the page loads
+const ALGORITHM_NAME = run.algorithmName;
+const IS_WEIGHTED = !board.unweightedAlgorithms.includes(ALGORITHM_NAME);
+weightedOptionElementsState(IS_WEIGHTED);
 
 // Event listener for run button
 let runButton = document.getElementById("run");
@@ -25,6 +47,7 @@ runButton.addEventListener("click", () => {
 
 // Event listener for the board layout options
 let layoutOptions = document.getElementById("layoutOptions").children;
+console.log(layoutOptions)
 layoutOptions = Array.from(layoutOptions);
 layoutOptions.forEach((option) => {
     option.addEventListener("click", (e) => {
@@ -59,5 +82,49 @@ clearBoardOptions.forEach((option) => {
 
         // Clear specific nodes
         board.clearBoard(clearOption);
+    });
+});
+
+// Event listener for the algorithm options
+let algorithmDropdown = document.getElementById("algorithmDropdown");
+let algorithmOptions = document.getElementById("algorithmOptions");
+algorithmOptions = Array.from(algorithmOptions.children);
+algorithmOptions.forEach((option) => {
+    option.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // Get the algorithm name
+        let algorithmName = option.innerText;
+        let StrippedAlgorithmName = algorithmName.toLowerCase().trim();
+        
+        // Check if the algorithm is weighted
+        let isWeighted = !board.unweightedAlgorithms.includes(StrippedAlgorithmName);
+        
+        // Update state of settings based on if the algorithm is weighted
+        weightedOptionElementsState(isWeighted);
+
+        // Delete all weights if the algorithm is unweighted
+        if(!isWeighted) {
+            board.clearWeights();
+        }
+
+        // Update the dropdown text to the algorithm name
+        algorithmDropdown.innerText = algorithmName;
+    });
+});
+
+// Event listener for the pencil options
+let pencilDropdown = document.getElementById("pencilDropdown");
+let pencilOptions = document.getElementById("pencilOptions");
+pencilOptions = Array.from(pencilOptions.children);
+pencilOptions.forEach((option) => {
+    option.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // Get the pencil name
+        let pencilName = option.innerText;
+
+        // Update the dropdown text to the pencil name
+        pencilDropdown.innerText = pencilName;
     });
 });
