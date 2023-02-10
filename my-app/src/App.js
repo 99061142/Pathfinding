2aee54c65ff36c060592e76f159228d9184aa09a
99061142 <xarbprive@gmail.com>
2023-02-10 20:32:09 +0100

    Import Board

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './nav.jsx'
+import Board from './board.jsx'
 
 function App() {
     return (
         <>
             <Navigation />
+            <Board />
         </>
     );
 }
