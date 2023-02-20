1874d3fbf2577a74bc4329b19c01e7a0eeb3a254
99061142 <xarbprive@gmail.com>
2023-02-20 20:44:13 +0100

    Set board states inside app component itself
    Add the states for the board inside the app component itself. Because of this the navigation component can read the values too.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,62 @@ import './app.css';
 import Navigation from './nav.jsx';
 import CellInformation from './cellInformation';
 import Board from './board.jsx';
+import { Component } from 'react';
 
-function App() {
-    return (
-        <>
-            <Navigation />
-            <CellInformation />
-            <Board />
-        </>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            startPos: null,
+            endPos: null,
+            running: false,
+            board: []
+        };
+    }
+
+    setEndPos = pos => {
+        this.setState({
+            endPos: pos
+        });
+    }
+
+    setStartPos = pos => {
+        this.setState({
+            startPos: pos,
+        });
+    }
+
+    setRunning = bool => {
+        this.setState({
+            startPos: bool,
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <Navigation 
+                    running={this.state.running}
+                    board={this.state.board}
+                    startPos={this.state.startPos} 
+                    endPos={this.state.endPos} 
+                    setStartPos={this.setStartPos} 
+                    setEndPos={this.setEndPos}
+                    setRunning={this.setRunning}
+                />
+                <CellInformation />
+                <Board
+                    running={this.state.running}
+                    board={this.state.board}
+                    startPos={this.state.startPos} 
+                    endPos={this.state.endPos} 
+                    setStartPos={this.setStartPos} 
+                    setEndPos={this.setEndPos}
+                    setRunning={this.setRunning}
+                />
+            </>
+        );
+    }
 }
 
 export default App;
