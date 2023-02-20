b237159e30b2753040224334d88efc89b88d9b29
99061142 <xarbprive@gmail.com>
2023-02-20 20:45:46 +0100

    Delete setters for the board

--- my-app/src/board.jsx
+++ my-app/src/board.jsx
@@ -5,36 +5,13 @@ class Board extends Component {
     constructor() {
         super();
         this.state = {
-            running: false,
-            startPos: null,
-            endPos: null,
-            board: [],
             rows: 0,
-            cells: 0
+            cells: 0,
         };
     }
 
     setContent(ROWS, CELLS) {
-        let board = [];
-        for (let rowIndex = 0; rowIndex <= ROWS; rowIndex++) {
-            board.push([]);
-            for (let cellIndex = 0; cellIndex <= CELLS; cellIndex++) {
-                const CELL = <Cell
-                    key={cellIndex}
-                    row={rowIndex}
-                    cell={cellIndex}
-                    running={this.algorithmRunning}
-                    getStartPos={this.getStartPos}
-                    setStartPos={this.setStartPos}
-                    getEndPos={this.getEndPos}
-                    setEndPos={this.setEndPos}
-                ></Cell>
-                board[rowIndex].push(CELL)
-            }
-        }
-
         this.setState({
-            board: board,
             rows: ROWS,
             cells: CELLS
         })
@@ -51,7 +28,7 @@ class Board extends Component {
     }
 
     getRows() {
-        // Get the amount of rows by the max height of the element divided by the cell height
+        // Get the amount of rows by the max height of the board divided by the cell height
         const BOARD = document.getElementById("board");
         const TOP = Math.round(BOARD.getBoundingClientRect().top);
         const MAX_HEIGHT = Math.round(window.innerHeight - TOP);
@@ -60,7 +37,7 @@ class Board extends Component {
     }
 
     getCells() {
-        // Get the amount of cells by the max width of the element divided by the cell width
+        // Get the amount of cells by the max width of the board divided by the cell width
         const MAX_WIDTH = Math.round(window.innerWidth);
         const CELLS = Math.round(MAX_WIDTH / 29);
         return CELLS
@@ -74,46 +51,28 @@ class Board extends Component {
         this.updateContent();
     }
 
-    getStartPos = () => {
-        let startPos = this.state.startPos;
-        return startPos
-    }
-
-    setStartPos = pos => {
-        this.setState({
-            startPos: pos
-        });
-    }
-
-    getEndPos = () => {
-        let endPos = this.state.endPos;
-        return endPos
-    }
-
-    setEndPos = pos => {
-        this.setState({
-            endPos: pos
-        });
-    }
-
-    algorithmRunning = () => {
-        let running = this.state.running;
-        return running
-    }
-
     render() {
         return (
             <table id="board" className="my-2 d-flex justify-content-center" >
                 <tbody>
-                    {[...Array(this.state.rows)].map((_, row) =>
-                        <tr key={row}>
-                            {[...Array(this.state.cells)].map((_, cell) =>
-                                this.state.board[row][cell]
+                    {[...Array(this.state.rows)].map((_, rowIndex) =>
+                        <tr key={rowIndex}>
+                            {[...Array(this.state.cells)].map((_, cellIndex) =>
+                                <Cell
+                                    key={cellIndex}
+                                    row={rowIndex}
+                                    cell={cellIndex}
+                                    running={this.props.running}
+                                    startPos={this.props.startPos}
+                                    setStartPos={this.props.setStartPos}
+                                    endPos={this.props.endPos}
+                                    setEndPos={this.props.setEndPos}
+                                />
                             )}
                         </tr>
                     )}
                 </tbody>
-            </table>
+            </table >
         );
     }
 }
