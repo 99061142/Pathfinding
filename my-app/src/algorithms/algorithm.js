38baccca4838bf8a018d06fa5d9c6aad0930c259
99061142 <xarbprive@gmail.com>
2023-02-23 13:38:58 +0100

    Functions to set cell to visited/next/fastest

--- my-app/src/algorithms/algorithm.js
+++ my-app/src/algorithms/algorithm.js
@@ -5,6 +5,20 @@ class Algorithm {
         this.props = props
     }
 
+    setVisited(row, cell) {
+        this.props.setCellData({name: 'visited'}, row, cell)
+    }
+
+    
+    setNext(row, cell) {
+        this.props.setCellData({name: 'next'}, row, cell)
+    }
+
+    
+    setFastest(row, cell) {
+        this.props.setCellData({name: 'fastest'}, row, cell)
+    }
+
     sleep() {
         let ms = 100 - this.props.speed;
         if(ms == 0) { return }
