02107b33cb883cc8eba530deb81892910bd7d152
99061142 <xarbprive@gmail.com>
2023-02-23 13:36:23 +0100

    Added sleep function

--- my-app/src/algorithms/algorithm.js
+++ my-app/src/algorithms/algorithm.js
@@ -1,4 +1,15 @@
+import { Component } from "react";
+
 class Algorithm {
+    constructor(props) {
+        this.props = props
+    }
+
+    sleep() {
+        let ms = 100 - this.props.speed;
+        if(ms == 0) { return }
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 }
 
 export default Algorithm;
