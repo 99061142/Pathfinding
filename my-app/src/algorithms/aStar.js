1db1823f87aec939cd7a9cfa1cdb430a7fe5990b
99061142 <xarbprive@gmail.com>
2023-02-23 18:43:03 +0100

    Made code cleaner

--- my-app/src/algorithms/aStar.js
+++ my-app/src/algorithms/aStar.js
@@ -25,25 +25,20 @@ class AStar extends Algorithm {
         // Cost from the position to the end
         const [ROW, CELL] = pos;
         const [END_ROW, END_CELL] = this.props.endPos;
-
         let rowCost = Math.abs(ROW - END_ROW);
         let cellCost = Math.abs(CELL - END_CELL);
+        let cost = this.cellWeight(pos) * 10; // Set cost as current weight of the node
 
-        // Set cost as current weight of the node
-        let cost = this.cellWeight(pos) * 10;
-
-        // Add 14 cost for each diagonal move and 10 cost for each horizontal or vertical move
+        // Add 14 cost for each diagonal move and 10 cost for each horizontal or vertical move, and return the total cost
         while(rowCost && cellCost) {
             rowCost--;
             cellCost--;
             cost += 14;
         }
-
         while(rowCost) {
             rowCost--;
             cost += 10;
         }
-
         while(cellCost) {
             cellCost--;
             cost += 10;
@@ -55,27 +50,22 @@ class AStar extends Algorithm {
         // Cost from the start to the position
         const [ROW, CELL] = pos;
         const [START_ROW, START_CELL] = this.props.startPos;
-
         let rowCost = Math.abs(ROW - START_ROW);
-        let colCost = Math.abs(CELL - START_CELL);
+        let cellCost = Math.abs(CELL - START_CELL);
+        let cost = this.cellWeight(pos) * 10; // Set cost as current weight of the node
 
-        // Set cost as current weight of the node
-        let cost = this.cellWeight(pos) * 10;
-
-        // Add 14 cost for each diagonal move and 10 cost for each horizontal or vertical move
-        while(rowCost && colCost) {
+        // Add 14 cost for each diagonal move and 10 cost for each horizontal or vertical move, adn return the total cost
+        while(rowCost && cellCost) {
             rowCost--;
-            colCost--;
+            cellCost--;
             cost += 14;
         }
-
         while(rowCost) {
             rowCost--;
             cost += 10;
         }
-
-        while(colCost) {
-            colCost--;
+        while(cellCost) {
+            cellCost--;
             cost += 10;
         }
         return cost;
@@ -98,7 +88,6 @@ class AStar extends Algorithm {
         // Get the position with the lowest f cost, or if the f costs are equal, the lowest h cost
         let lowestFCost = Infinity;
         let lowestFCostPos = null;
-
         for(let pos of this._queue) {
             let fCost = this.fCost(pos);
             if(fCost < lowestFCost || fCost == lowestFCost && this.hCost(pos) < this.hCost(lowestFCostPos)) {
@@ -110,47 +99,45 @@ class AStar extends Algorithm {
     }
 
     dequeue(pos) {
-        let index = this._queue.indexOf(pos);
-        this._queue.splice(index, 1);
+        const INDEX = this._queue.indexOf(pos);
+        this._queue.splice(INDEX, 1);
     }
 
     async run() {
         while(this._queue && this._queue.length) {
-            let queuedPos = this.head;
-            this.dequeue(queuedPos);
+            const QUEUED_POS = this.head;
+            this.dequeue(QUEUED_POS);
 
-            for(let direction of this._directions) {
-                let pos = this.position(queuedPos, direction);
-        //        let stringifiedPosition = position.toString();
+            for(const DIRECTION of this._directions) {
+                const POS = this.position(QUEUED_POS, DIRECTION);
 
                 // If the position is not movable, skip
-                if(!this.canMove(pos)) { continue; }
+                if(!this.canMove(POS)) { continue; }
 
                 // Get the costs of the position
-                let g = this.gCost(pos);
-                let h = this.hCost(pos);
-                let f = g + h;
+                const G = this.gCost(POS);
+                const H = this.hCost(POS);
+                const F = G + H;
 
                 // If the position is already in the path, and the new cost is higher than the old cost, or if the position is visited, skip
-                if(this._path[pos] && this._path[pos].f < f) { continue }
+                if(this._path[POS] && this._path[POS].f < F) { continue }
 
                 // If the position is not in the path, add it
-                if(this._path[pos] === undefined) {
-                    this._path[pos] = {};
+                if(this._path[POS] === undefined) {
+                    this._path[POS] = {};
 
                     // Add the position to the queue and visited list
-                    this._queue.push(pos);
+                    this._queue.push(POS);
 
-                    if(!this.isEnd(pos)) {
-                        this.setNext(pos);
+                    if(!this.isEnd(POS)) {
+                        this.setNext(POS);
                     }
                 }
                 // Add position costs to the path
-                this._path[pos].parent = queuedPos;
-                this._path[pos].g = g;
-                this._path[pos].h = h;
+                this._path[POS].parent = QUEUED_POS;
+                this._path[POS].g = G;
+                this._path[POS].h = H;
             }
-
             // If it can't search any further, break
             if(this.head == null) { return }
 
