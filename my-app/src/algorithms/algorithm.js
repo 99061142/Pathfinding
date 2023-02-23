5c0073c58047397bcd3105f329f1c3426c1fdb51
99061142 <xarbprive@gmail.com>
2023-02-23 14:59:41 +0100

    Functions to check/set algo cells in every algo

--- my-app/src/algorithms/algorithm.js
+++ my-app/src/algorithms/algorithm.js
@@ -5,24 +5,73 @@ class Algorithm {
         this.props = props
     }
 
-    setVisited(row, cell) {
-        this.props.setCellData({name: 'visited'}, row, cell)
+    isStart(pos) {
+        const [ROW, CELL] = pos;
+        const [START_ROW, START_CELL] = this.props.startPos;
+        const IS_START = ROW === START_ROW && CELL === START_CELL;
+        return IS_START
+    }
+
+    isEnd(pos) {
+        const [ROW, CELL] = pos;
+        const [END_ROW, END_CELL] = this.props.endPos;
+        const IS_END = ROW === END_ROW && CELL === END_CELL;
+        return IS_END
+    }
+
+    canMove(pos) {
+        const [ROW, CELL] = pos;
+        const MAX_ROWS = this.props.board.length;
+        const MAX_CELLS = this.props.board[0].length;
+
+        if(ROW < 0 || CELL < 0 || ROW >= MAX_ROWS || CELL >= MAX_CELLS) { 
+            return false
+        }
+
+        const CELL_DATA = this.props.board[ROW][CELL];
+        const CELL_NAME = CELL_DATA.name;
+        const CAN_MOVE = CELL_NAME === '' || this.isEnd(pos);
+        return CAN_MOVE
+    }
+
+    position(position, direction) {
+        const [POS_ROW, POS_CELL] = position;
+        const [DIR_ROW, DIR_CELL] = direction;
+        const ROW = POS_ROW + DIR_ROW;
+        const CELL = POS_CELL + DIR_CELL;
+        position = [ROW, CELL];
+        return position
+    }
+
+    async setVisited(pos) {
+        const [ROW, CELL] = pos;
+        this.props.setCellData({name: 'visited'}, ROW, CELL);
+        await this.sleep();
     }
 
     
-    setNext(row, cell) {
-        this.props.setCellData({name: 'next'}, row, cell)
+    setNext(pos) {
+        const [ROW, CELL] = pos;
+        this.props.setCellData({name: 'next'}, ROW, CELL);
     }
 
     
-    setFastest(row, cell) {
-        this.props.setCellData({name: 'fastest'}, row, cell)
+    async setFastest(pos) {
+        const [ROW, CELL] = pos;
+        this.props.setCellData({name: 'fastest'}, ROW, CELL);
+        this.sleep();
     }
 
     sleep() {
         let ms = 100 - this.props.speed;
         if(ms == 0) { return }
         return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async showRoute(route) {
+        for(let position of route) {
+            await this.setFastest(position);
+        }
     }
 }
 
