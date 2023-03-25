c50af7b6ae8d69b7c6431817d3ad40fb46e5e2af
99061142 <xarbprive@gmail.com>
2023-03-25 22:46:48 +0100

    Delete unnecessary functions

--- src/cell.jsx
+++ src/cell.jsx
@@ -15,18 +15,6 @@ class Cell extends Component {
         this.props.setCellData(this.pos, DATA);
     }
 
-    getType() {
-        const DATA = this.props.board[this.row][this.col];
-        const TYPE = DATA.type;
-        return TYPE
-    }
-
-    getWeight() {
-        const DATA = this.props.board[this.row][this.col];
-        const WEIGHT = DATA.weight;
-        return WEIGHT
-    }
-
     hover() {
 
     }
@@ -36,8 +24,9 @@ class Cell extends Component {
     }
 
     render() {
-        const TYPE = this.getType() ? this.getType() : '';
-        const WEIGHT = this.getWeight() ? this.getWeight() : 1;
+        const DATA = this.props.board[this.row][this.col];
+        const TYPE = DATA.type ? DATA.type : '';
+        const WEIGHT = DATA.weight ? DATA.weight : 1;
         return (
             <td
                 ref={this.element}
