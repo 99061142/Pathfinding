665fbdf564b659db682eca332f637263e98498ab
99061142 <xarbprive@gmail.com>
2023-03-25 22:46:21 +0100

    Update cellIcon.jsx

--- src/cellIcon.jsx
+++ src/cellIcon.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faHouse, faWeightHanging } from '@fortawesome/free-solid-svg-icons'
 
 function CellIcon({ type = '', weight = 1 }) {
-    if ((type === '' && weight <= 1)) {
+    if ((type === '' && weight === 1)) {
         return null
     }
     if (weight > 1 && type === '') {
