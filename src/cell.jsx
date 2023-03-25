60f513d200a449f0332ef7ca95a3d3ab92965846
99061142 <xarbprive@gmail.com>
2023-03-25 17:27:14 +0100

    Delete functionality for the cells

--- src/cell.jsx
+++ src/cell.jsx
@@ -1,98 +1,35 @@
-import { Component } from "react";
-import { faArrowRight, faHouse, faWeightHanging } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { createRef } from "react";
+import { Component, createRef } from "react";
+import CellIcon from './cellIcon'
 
 class Cell extends Component {
     constructor({ row, cell }) {
         super();
         this.row = row;
-        this.cell = cell;
-        this.element = createRef();
-        this.initName = '';
-        this.initWeight = 1;
+        this.col = cell;
+        this.cell = createRef();
     }
 
-    componentDidMount() {
-        const ELEMENT = this.element.current;
-        const DATA = { ...ELEMENT.dataset };
-        this.props.setCellData(DATA, this.row, this.cell);
-    }
+    hover() {
 
-    updateState(name) {
-        /* Update the state of the element by the pencil value, or the given 'name' parameter when it is defined.
-        When the pencil has an weight value, set the weight as dataset weight. */
-        let [pencilValue, pencilWeight] = document.getElementById('pencil').value.toLowerCase().split('-');
-        if (name) {
-            pencilValue = name
-        }
-
-        let data
-        if (pencilValue === "erase") {
-            data = {
-                name: this.initName,
-                weight: this.initWeight
-            };
-            this.props.setCellData(data, this.row, this.cell);
-            return
-        }
-
-        data = {
-            name: pencilValue
-        };
-        if (pencilWeight) {
-            data.weight = pencilWeight;
-        }
-        this.props.setCellData(data, this.row, this.cell);
     }
 
     clicked() {
-        let cellData = this.props.board[this.row][this.cell];
-        let name = cellData.name;
 
-        // If the algorithm is running, return
-        if (this.props.runnin || name === "start" || name === "end") { return }
-
-        // If the element isn't empty, set it empty
-        if (name !== '') {
-            this.updateState('erase');
-            return
-        }
-        // Set element to given pencil value
-        this.updateState();
-    }
-
-    hover(element) {
-        // If the algorithm is running, return
-        if (this.props.running) { return }
-
-        // If the element is clicked, update name
-        let clicked = element.buttons === 1;
-        if (clicked) {
-            this.clicked();
-        }
     }
 
     render() {
-        let cellData = this.props.board[this.row][this.cell];
-        let name = cellData.name ? cellData.name : this.initName;
-        let weight = cellData.weight ? cellData.weight : this.initWeight;
+        let cellData = this.props.board[this.row][this.col];
         return (
             <td
-                ref={this.element}
-                data-name={name}
-                data-weight={weight}
-                className={`border border-dark cell ${name}`}
+                ref={this.cell}
+                data-name={cellData.type}
+                data-weight={cellData.weight}
+                className={`border border-dark cell ${cellData.type}`}
                 onClick={() => this.clicked()}
-                onMouseEnter={(element) => this.hover(element)}
+                onMouseEnter={(e) => this.hover(e)}
             >
-                {
-                    name === "start" ? <FontAwesomeIcon icon={faArrowRight} />
-                        : name === "end" ? <FontAwesomeIcon icon={faHouse} />
-                            : weight > 1 ? <FontAwesomeIcon icon={faWeightHanging} />
-                                : null
-                }
-            </td>
+                {<CellIcon type={cellData.type} weight={cellData.weight} />}
+            </td >
         );
     }
 }
