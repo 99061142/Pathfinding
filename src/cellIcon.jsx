02a3a42137e439d17f36fbcca808edd13b15efe0
99061142 <xarbprive@gmail.com>
2023-03-25 17:26:43 +0100

    Function to get the icon for the cell if needed

---
+++ src/cellIcon.jsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faHouse, faWeightHanging } from '@fortawesome/free-solid-svg-icons'
+
+function CellIcon({ type, weight }) {
+    if ((type === '' && weight <= 1)) {
+        return null
+    }
+    if (weight > 1 && type === '') {
+        type = "weight";
+    }
+
+    const ICONS = {
+        "start": faArrowRight,
+        "end": faHouse,
+        "weight": faWeightHanging
+    };
+    const ICON = ICONS[type];
+
+    if (!ICON) {
+        return null
+    }
+    return <FontAwesomeIcon icon={ICON} />
+}
+
+export default CellIcon;
