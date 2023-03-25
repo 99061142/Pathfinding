49ab24fa63c8b59f4dbd36fab548cbcfb84b7123
99061142 <xarbprive@gmail.com>
2023-03-25 17:50:23 +0100

    Map over list with types to show all types

--- src/cellInformation.jsx
+++ src/cellInformation.jsx
@@ -1,45 +1,19 @@
-import { faArrowRight, faHouse, faWeightHanging } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container, Row, Col } from "react-bootstrap";
+import CellIcon from './cellIcon';
 
 function CellInformation() {
+    const TYPES = ["start", "end", "weight", "wall", "visited", "next", "fastest"];
     return (
-        <Container className="cellInformation" fluid>
+        <Container fluid>
             <Row className="justify-content-around text-center">
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark start cell mx-auto">
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </div>
-                    <p>Start</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark end cell mx-auto">
-                        <FontAwesomeIcon icon={faHouse} />
-                    </div>
-                    <p>End</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark weight cell mx-auto">
-                        <FontAwesomeIcon icon={faWeightHanging} />
-                    </div>
-                    <p>Weight</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark wall cell mx-auto"></div>
-                    <p>Wall</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark visited cell mx-auto"></div>
-                    <p>Visited</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark next cell mx-auto"></div>
-                    <p>Next</p>
-                </Col>
-                <Col xs={4} sm={3} md="auto" className="my-3">
-                    <div className="border border-dark fastest cell mx-auto"></div>
-                    <p>Fastest</p>
-                </Col>
+                {TYPES.map((type, key) =>
+                    <Col xs={4} sm={3} md="auto" className="my-3" key={key}>
+                        <div style={{ maxWidth: '24px' }} className={`border border-dark cell mx-auto ${type}`}>
+                            <CellIcon type={type} />
+                        </div>
+                        <p style={{ textTransform: 'capitalize' }}>{type}</p>
+                    </Col>
+                )}
             </Row>
         </Container>
     );
