b9e9c53929e0c2f094b9fcc5befa524170b5ca09
99061142 <xarbprive@gmail.com>
2023-03-25 18:49:32 +0100

    Add the cell data inside the board when mounted

--- src/cell.jsx
+++ src/cell.jsx
@@ -2,21 +2,28 @@ import { Component, createRef } from "react";
 import CellIcon from './cellIcon'
 
 class Cell extends Component {
-    constructor({ row, cell }) {
+    constructor({ row, col }) {
         super();
         this.row = row;
-        this.col = cell;
+        this.col = col;
+        this.pos = [row, col];
+        this.element = createRef();
+    }
+
+    componentDidMount() {
+        const DATA = { ...this.element.current.dataset };
+        this.props.setCellData(this.pos, DATA);
     }
 
     getType() {
         const DATA = this.props.board[this.row][this.col];
-        const TYPE = DATA.type
+        const TYPE = DATA.type;
         return TYPE
     }
 
     getWeight() {
         const DATA = this.props.board[this.row][this.col];
-        const WEIGHT = DATA.weight
+        const WEIGHT = DATA.weight;
         return WEIGHT
     }
 
@@ -29,16 +36,19 @@ class Cell extends Component {
     }
 
     render() {
+        const TYPE = this.getType() ? this.getType() : '';
+        const WEIGHT = this.getWeight() ? this.getWeight() : 1;
         return (
             <td
-                data-type={this.getType()}
-                data-weight={this.getWeight()}
-                className={`border border-dark cell ${this.getType()}`}
+                ref={this.element}
+                data-type={TYPE}
+                data-weight={WEIGHT}
+                className={`border border-dark cell ${TYPE}`}
                 onClick={() => this.clicked()}
                 onMouseEnter={(e) => this.hover(e)}
             >
-                {<CellIcon type={this.getType()} weight={this.getWeight()} />}
-            </td >
+                {<CellIcon type={TYPE} weight={WEIGHT} />}
+            </td>
         );
     }
 }
