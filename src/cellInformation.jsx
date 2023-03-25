15784fa3f43be3fdf8b240b0fdbd56b7249881b0
99061142 <xarbprive@gmail.com>
2023-03-25 19:01:23 +0100

    Same as previous commit, but on parent element

--- src/cellInformation.jsx
+++ src/cellInformation.jsx
@@ -4,10 +4,10 @@ import CellIcon from './cellIcon';
 function CellInformation() {
     const TYPES = ["start", "end", "weight", "wall", "visited", "next", "fastest"];
     return (
-        <Container fluid>
+        <Container fluid className="d-none d-sm-block">
             <Row className="justify-content-around text-center">
                 {TYPES.map((type, key) =>
-                    <Col sm="auto" className="my-3 d-none d-sm-block" key={key}>
+                    <Col sm="auto" className="my-3" key={key}>
                         <div style={{ maxWidth: '24px' }} className={`border border-dark cell mx-auto ${type}`}>
                             <CellIcon type={type} />
                         </div>
