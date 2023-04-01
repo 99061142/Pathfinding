eae71b0c4e94a924dea2315b48fe4d79862ebbef
99061142 <xarbprive@gmail.com>
2023-04-01 03:24:54 +0200

    Change styling to classname

--- src/cellInformation.jsx
+++ src/cellInformation.jsx
@@ -2,16 +2,16 @@ import { Container, Row, Col } from "react-bootstrap";
 import CellIcon from './cellIcon';
 
 function CellInformation() {
-    const TYPES = ["start", "end", "weight", "wall", "visited", "next", "fastest"];
+    const TYPES = ['start', 'end', 'weight', 'wall', 'visited', 'next', 'fastest'];
     return (
         <Container fluid className="d-none d-sm-block">
             <Row className="justify-content-around text-center">
                 {TYPES.map((type, key) =>
-                    <Col sm="auto" className="my-3" key={key}>
+                    <Col className="my-3" key={key}>
                         <div className={`border border-dark cell mx-auto ${type}`}>
                             <CellIcon type={type} />
                         </div>
-                        <p style={{ textTransform: 'capitalize' }}>{type}</p>
+                        <p className="mb-0 text-capitalize">{type}</p>
                     </Col>
                 )}
             </Row>
