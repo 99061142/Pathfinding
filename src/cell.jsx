32519890de89cb8d7304db681581be8bc01a178f
99061142 <xarbprive@gmail.com>
2023-04-05 19:09:19 +0200

    Add funcs to update cells when needed + readable

--- src/cell.jsx
+++ src/cell.jsx
@@ -2,38 +2,102 @@ import { Component, createRef } from "react";
 import CellIcon from './cellIcon'
 
 class Cell extends Component {
-    constructor({ row, col }) {
+    constructor({ row, col, isStart, isEnd }) {
         super();
+        const TYPE = isStart ? 'start' : isEnd ? 'end' : '';
+        this.state = {
+            type: TYPE,
+            weight: 1
+        };
         this.pos = [row, col];
         this.element = createRef();
-        this.getsDragged = false;
-    }
-
-    getDataset() {
-        const ELEMENT = this.element.current;
-        const DATASET = ELEMENT.dataset;
-        return DATASET
-    }
-
-    getType() {
-        const DATASET = this.getDataset();
-        const TYPE = DATASET.type;
-        return TYPE
     }
 
     componentDidMount() {
-        const DATASET = { ...this.getDataset() };
-        this.props.setCellData(this.pos, DATASET);
+        // Add the functionality to update the cell when needed
+        const DATA = {
+            setDataset: this.setDataset,
+            getType: this.getType,
+            setType: this.setType,
+            getWeight: this.getWeight,
+            setWeight: this.setWeight
+        }
+        this.props.setCellData(this.pos, DATA);
+
+        // Add the local start pos when it's the start pos
+        if (this.isStartPos()) {
+            this.props.setStartPos(this.pos);
+        }
+        // Add the local end pos when it's the end pos
+        else if (this.isEndPos()) {
+            this.props.setEndPos(this.pos);
+        }
+    }
+
+    setDataset = dataset => {
+        // Set every data value that can be a number to a number
+        for (let [key, value] of Object.entries(dataset)) {
+            if (parseInt(value)) {
+                dataset[key] = Number(value);
+            }
+        }
+        // Merge the new data with the old data and set the data
+        const OLD_DATASET = this.state.dataset;
+        dataset = Object.assign({}, OLD_DATASET, dataset)
+        this.setState({
+            type: dataset.type,
+            weight: dataset.weight
+        });
+
+        // If the dataset has the type 'start', update the local start pos
+        if (dataset.type === 'start') {
+            this.props.setStartPos(this.pos);
+        }
+        // If the dataset has the type 'end', update the local end pos
+        else if (dataset.type === 'end') {
+            this.props.setEndPos(this.pos);
+        }
+    }
+
+    getWeight = () => {
+        const WEIGHT = this.state.weight;
+        return WEIGHT
+    }
+
+    setWeight = weight => {
+        this.setState({
+            weight
+        });
+    }
+
+    getType = () => {
+        const TYPE = this.state.type;
+        return TYPE
+    }
+
+    setType = type => {
+        this.setState({
+            type
+        });
+
+        // If the dataset has the type 'start', set the pos as start pos
+        if (type === 'start') {
+            this.props.setStartPos(this.pos);
+        }
+        // If the dataset has the type 'end', set the pos as end pos
+        else if (type === 'end') {
+            this.props.seEndPos(this.pos);
+        }
     }
 
     isStartPos() {
-        const TYPE = this.getType();
-        return TYPE === "start"
+        const IS_START_POS = this.getType() === 'start';
+        return IS_START_POS;
     }
 
     isEndPos() {
-        const TYPE = this.getType();
-        return TYPE === "end"
+        const IS_END_POS = this.getType() === 'end';
+        return IS_END_POS;
     }
 
     hover(e) {
@@ -48,8 +112,8 @@ class Cell extends Component {
     }
 
     clicked() {
-        // If the algorithm is running or the cell gets dragged, return
-        if (this.props.running || this.getsDragged) { return }
+        // If the algorithm is running, return
+        if (this.props.running) { return }
 
         // Get the pencil type and weight
         let [pencilType, pencilWeight] = document.getElementById('pencil').value.split('-');
@@ -65,7 +129,7 @@ class Cell extends Component {
             type: pencilType,
             weight: pencilWeight
         };
-        this.props.setCellData(this.pos, DATA);
+        this.setDataset(DATA);
     }
 
     dragOver(e) {
@@ -73,60 +137,45 @@ class Cell extends Component {
         if (this.isStartPos() || this.isEndPos()) {
             return
         }
-
         // Allow the drop
         e.preventDefault();
     }
 
     dragStart(e) {
         // Only allow the start or end pos to be dragged IF the algorithm isn't running
-        if (!this.isStartPos() && !this.isEndPos() || this.props.running) {
+        if ((!this.isStartPos() && !this.isEndPos()) || this.props.running) {
             e.preventDefault();
             return
         }
-
-        // Set the cell as dragged, and add the cell id as datatransfer
-        this.getsDragged = true;
         e.dataTransfer.setData("id", e.target.id);
     }
 
     dragEnd() {
-        // Unset the cell as dragged
-        this.getsDragged = false;
-    }
-
-    dragDrop(e) {
-        // Add the new start / end data
-        const DRAG_TARGET_ID = e.dataTransfer.getData('id');
-        const DRAG_TARGET = document.getElementById(DRAG_TARGET_ID);
-        let dragTargetData = DRAG_TARGET.dataset;
-        this.props.setCellData(this.pos, dragTargetData);
-
-        // Remove the old start / end data
-        const DRAG_TARGET_POS = DRAG_TARGET_ID === 'start' ? this.props.startPos : this.props.endPos;
-        dragTargetData = {
+        // Remove the dragged data from the dragged cell
+        const DATA = {
             type: '',
             weight: 1
         };
-        this.props.setCellData(DRAG_TARGET_POS, dragTargetData);
+        this.setDataset(DATA);
+    }
 
-        // Set the new start / end pos
-        if (DRAG_TARGET_ID === 'start') {
-            this.props.setStartPos(this.pos)
-            return
-        }
-        this.props.setEndPos(this.pos)
+    dragDrop(e) {
+        // Add the dragged data on the dropped cell
+        const DRAG_TARGET_ID = e.dataTransfer.getData('id');
+        const DRAG_TARGET = document.getElementById(DRAG_TARGET_ID);
+        const DATA = DRAG_TARGET.dataset;
+        this.setDataset(DATA);
     }
 
     render() {
-        const DATA = this.props.data;
-        const TYPE = DATA.type || '';
-        const WEIGHT = DATA.weight || 1;
+        const TYPE = this.state.type;
+        const WEIGHT = this.state.weight
+        const ID = TYPE === 'start' || TYPE === 'end' ? TYPE : null;
         const DRAGGABLE = (TYPE === 'start' || TYPE === 'end') && !this.props.running;
         return (
             <td
                 ref={this.element}
-                id={TYPE === 'start' || TYPE === 'end' ? TYPE : null}
+                id={ID}
                 className={`border border-dark cell ${TYPE}`}
                 data-type={TYPE}
                 data-weight={WEIGHT}
