c4bcaba74e52f4865850d274c38654484db0f46e
99061142 <xarbprive@gmail.com>
2023-04-10 19:06:36 +0200

    Delete weights too

--- src/clear/clearAll.js
+++ src/clear/clearAll.js
@@ -2,7 +2,8 @@ async function ClearAll(cells) {
     for (const cellsRow of cells) {
         for (const cell of cellsRow) {
             const CELL_TYPE = cell.getType();
-            if (CELL_TYPE === '' || CELL_TYPE === 'start' || CELL_TYPE === 'end') { continue }
+            const CELL_WEIGHT = cell.getWeight();
+            if ((CELL_TYPE === '' &&  CELL_WEIGHT === 1) || CELL_TYPE === 'start' || CELL_TYPE === 'end') { continue }
             cell.setType('');
             if(cell.getWeight() !== 1) {
                 cell.setWeight(1);
