833b75c60553fc29eb0af6a608f8f7e9a009a3a2
99061142 <xarbprive@gmail.com>
2023-04-10 14:42:38 +0200

    Function to create random weights on board

---
+++ src/layouts/randomWeights.js
@@ -0,0 +1,18 @@
+import ClearAll from "../clear/clearAll";
+
+async function RandomWeights(cells) {
+    // CLear the board before creating a new layout
+    await ClearAll(cells);
+
+    for (const cellsRow of cells) {
+        for (const cell of cellsRow) {
+            // If the percentage is higher than .33, continue
+            const PERCENAGE = Math.random();
+            if (PERCENAGE > .33 || cell.getType() !== '') { continue }
+            cell.setType('weight');
+            cell.setWeight(10);
+        }
+    }
+}
+
+export default RandomWeights;
