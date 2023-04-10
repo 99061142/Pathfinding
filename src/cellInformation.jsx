8bf4e0b26a80a55a62bc051818af526c96c24004
99061142 <xarbprive@gmail.com>
2023-04-10 15:11:22 +0200

    Moved styling between elements

--- src/cellInformation.jsx
+++ src/cellInformation.jsx
@@ -4,8 +4,8 @@ import CellIcon from './cellIcon';
 function CellInformation() {
     const TYPES = ['start', 'end', 'weight', 'wall', 'visited', 'next', 'fastest'];
     return (
-        <Container fluid className="d-none d-sm-block">
-            <Row className="my-3 justify-content-around text-center">
+        <Container fluid className="my-3 d-none d-sm-block">
+            <Row className="justify-content-around text-center">
                 {TYPES.map((type, key) =>
                     <Col key={key}>
                         <div className={`border border-dark cell mx-auto ${type}`}>
