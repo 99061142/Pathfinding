5eb4aa91f1f975a7bb8dedb7bf74ec0fe996a35e
99061142 <xarbprive@gmail.com>
2023-04-24 01:18:44 +0200

    Delete setCellData / seCells function
    * Changed class to function.
    * Deleted function to add a cell the board state.
    * Deleted function to set the board state.
    * Deleted cells/setCellData props.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,22 @@
 import './app.css';
-import { Component } from 'react';
-import Settings from './settings.jsx';
+import { useState } from 'react';
+import Settings from './settings';
 import CellInformation from './cellInformation';
-import Board from './board.jsx';
+import Board from './board';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            running: false,
-            cells: []
-        };
-    }
-
-    setRunning = bool => {
-        this.setState({
-            running: bool
-        });
-    }
-
-    setCellData = (pos, data) => {
-        const [ROW, COL] = pos;
-        let cells = this.state.cells;
-
-        // If the row isnt created yet, create it
-        if(cells.length === ROW) {
-            cells.push([]);
-        }
-
-        // Add the cell to the cells
-        cells[ROW][COL] = data;
-        this.setCells(cells);
-    }
-
-    setCells(cells) {
-        this.setState({
-            cells
-        });
-    }
-
-    render() {
-        const PROPS = {
-            running: this.state.running,
-            cells: this.state.cells,
-            setRunning: this.setRunning,
-            setCellData: this.setCellData
-        };
-        return (
-            <>
-                <Settings {...PROPS} />
-                <CellInformation />
-                <Board {...PROPS} />
-            </>
-        );
-    }
+function App() {
+    const [running, setRunning] = useState(false);
+    const PROPS = {
+        running,
+        setRunning
+    };
+    return (
+        <>
+            <Settings {...PROPS} />
+            <CellInformation />
+            <Board {...PROPS} />
+        </>
+    );
 }
 
 export default App;
