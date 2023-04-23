90941b780b1678825b6443e79a77b4715531f9d5
99061142 <xarbprive@gmail.com>
2023-04-24 01:21:10 +0200

    Update type/weight state when datasets get updated
    * Update type state when the dataset type gets updated.
    * Update weight state when the dataset weight gets updated.
    * Changed class to function.
    * Made code cleaner.

--- src/cell.jsx
+++ src/cell.jsx
@@ -1,157 +1,139 @@
-import { Component, createRef } from "react";
+import { createRef, useEffect, useState } from "react";
 import CellIcon from './cellIcon'
-
 import Run from './run'
 
-class Cell extends Component {
-    constructor({ type, row, col }) {
-        super();
-        this.state = {
-            type,
-            weight: 1
-        };
-        this.pos = [row, col];
-        this.element = createRef();
-    }
+function Cell({ initType, initWeight, row, col, running, setRunning }) {
+    const element = createRef(null);
+    const pos = [row, col];
+    const [type, setType] = useState(initType);
+    const [weight, setWeight] = useState(initWeight);
 
-    componentDidMount() {
-        // Add the functionality to update the cell when needed
-        const DATA = {
-            getType: this.getType,
-            setType: this.setType,
-            getWeight: this.getWeight,
-            setWeight: this.setWeight
-        }
-        this.props.setCellData(this.pos, DATA);
-    }
-
-    getWeight = () => {
-        const WEIGHT = this.state.weight;
-        return WEIGHT
-    }
-
-    setWeight = weight => {
-        this.setState({
-            weight
+    useEffect(() => {
+        // When a state attribute gets updated, set the state as the updated value
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                const MUTATED_ATTRIBUTE_NAME = mutation.attributeName;
+                switch (MUTATED_ATTRIBUTE_NAME) {
+                    case 'data-type':
+                        const DATASET_TYPE = mutation.target.dataset.type;
+                        setType(DATASET_TYPE);
+                        break
+                    case 'data-weight':
+                        const DATASET_WEIGHT = Number(mutation.target.dataset.weight);
+                        setWeight(DATASET_WEIGHT);
+                        break
+                    default:
+                        throw Error(`The attribute "${MUTATED_ATTRIBUTE_NAME}" doesn't have a state that needs to be updated`);
+                }
+            }
         });
-    }
 
-    getType = () => {
-        const TYPE = this.state.type;
-        return TYPE
-    }
-
-    setType = type => {
-        this.setState({
-            type
+        observer.observe(element.current, {
+            attributeFilter: ['data-type', 'data-weight'],
         });
-    }
+    }, [element]);
 
-    isStartPos() {
-        const IS_START_POS = this.getType() === 'start';
-        return IS_START_POS;
-    }
-
-    isEndPos() {
-        const IS_END_POS = this.getType() === 'end';
-        return IS_END_POS;
-    }
-
-    hover(e) {
-        // If the algorithm is running, return
-        if (this.props.running) { return }
+    const hover = (e) => {
+        // If the algorithm is running or the cell is the start / end pos, return
+        if (running || type === 'start' || type === 'end') { return }
 
         // When the user left clicks on the cell, set the pencil value to the cell
-        const LEFT_CLICKED = e.buttons === 1;
-        if (LEFT_CLICKED) {
-            this.clicked();
+        const LEFT_CLICK = e.buttons === 1;
+        if (LEFT_CLICK) {
+            clicked();
         }
     }
 
-    clicked() {
+    const clicked = () => {
         // If the algorithm is running or the cell is the start / end pos, return
-        if (this.props.running || this.isStartPos() || this.isEndPos()) { return }
+        if (running || type === 'start' || type === 'end') { return }
 
-        // Set the pencil styling to the cell
+        // If the pencil type and/or weight is different than the current type and/or weight, update it
         let [pencilType, pencilWeight] = document.getElementById('pencil').value.split('-');
         pencilType = pencilType !== "weight" ? pencilType : '';
-        this.setType(pencilType);
+        if (pencilType !== type) {
+            element.current.dataset.type = pencilType;
+        }
         pencilWeight = Number(pencilWeight) || 1;
-        this.setWeight(pencilWeight);
+        if (pencilWeight !== weight) {
+            element.current.dataset.weight = pencilWeight;
+        }
     }
 
-    dragOver(e) {
+    const dragOver = (e) => {
         // Don't allow the drop when the user hovers over the start or end pos
-        if (this.isStartPos() || this.isEndPos()) {
+        if (type === 'start' || type === 'end') {
             return
         }
         // Allow the drop
         e.preventDefault();
     }
 
-    dragStart(e) {
-        // Only allow the start or end pos to be dragged if the algorithm isn't running
-        if ((!this.isStartPos() && !this.isEndPos()) || this.props.running) {
+    const dragStart = (e) => {
+        // Don't allow the drag when it isn't the start or end cell, or the algorithm is running
+        if ((type !== 'start' && type !== 'end') || running) {
             e.preventDefault();
             return
         }
-        e.dataTransfer.setData('id', e.target.id);
+        // Set the needed data to get it when the cell gets dropped
+        const TYPE = e.target.dataset.type;
+        e.dataTransfer.setData('type', TYPE);
+        const WEIGHT = e.target.dataset.weight;
+        e.dataTransfer.setData('weight', WEIGHT);
     }
 
-    async dragEnd() {
-        // If the start / end pos wasn't moved, return
-        const TYPE = this.getType();
-        const POSITIONS = document.querySelectorAll(`[data-type=${TYPE}]`)
+    const dragEnd = () => {
+        // If the cell wasn't moved, return
+        const POSITIONS = document.querySelectorAll(`td.${type}`)
         if (POSITIONS.length === 1) {
             return
         }
         // Remove the old start / end pos
-        this.setType('');
-        this.setWeight(1);
+        element.current.dataset.type = '';
+        element.current.dataset.weight = 1;
 
         // Run the algorithm when there is a path on the board
         const HAS_PATH = document.querySelector('td.next, td.visited, td.fastest');
         if (!HAS_PATH) { return }
-        await Run({
-            cells: this.props.cells,
-            setRunning: this.props.setRunning,
-            skip: true,
-            setCellData: this.props.setCellData
+        Run({
+            setRunning: setRunning,
+            skip: true
         });
     }
 
-    async dragDrop(e) {
-        // Move the start or end pos to the dropped cell
-        const DRAG_TARGET_ID = e.dataTransfer.getData('id');
-        const DRAG_TARGET = document.getElementById(DRAG_TARGET_ID);
-        const DATA = DRAG_TARGET.dataset;
-        this.setType(DATA.type);
-        this.setWeight(DATA.weight);
+    const dragDrop = (e) => {
+        // Set the type and weight as the dragged cell (if it isn't already)
+        const DROPPED_TYPE = e.dataTransfer.getData('type');
+        if (DROPPED_TYPE !== type) {
+            element.current.dataset.type = DROPPED_TYPE;
+        }
+        const DROPPED_WEIGHT = Number(e.dataTransfer.getData('weight'));
+        if (DROPPED_WEIGHT !== weight) {
+            element.current.dataset.type = DROPPED_WEIGHT;
+        }
     }
 
-    render() {
-        return (
-            <td
-                ref={this.element}
-                id={this.pos.join('-')}
-                className={`border border-dark cell ${this.state.type}`}
-                data-type={this.state.type}
-                data-weight={this.state.weight}
-                onClick={() => this.clicked()}
-                onMouseEnter={(e) => this.hover(e)}
-                draggable={(this.isStartPos() || this.isEndPos()) && !this.props.running}
-                onDragStart={(e) => this.dragStart(e)}
-                onDragOver={(e) => this.dragOver(e)}
-                onDrop={(e) => this.dragDrop(e)}
-                onDragEnd={() => this.dragEnd()}
-            >
-                <CellIcon
-                    type={this.state.type}
-                    weight={this.state.weight}
-                />
-            </td>
-        );
-    }
+    return (
+        <td
+            ref={element}
+            id={pos.join('-')}
+            className={`border border-dark cell ${type}`}
+            data-type={initType}
+            data-weight={initWeight}
+            onClick={clicked}
+            onMouseEnter={hover}
+            draggable={(type === 'start' || type === 'end') && !running}
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+            onDrop={dragDrop}
+            onDragEnd={dragEnd}
+        >
+            <CellIcon
+                type={type}
+                weight={weight}
+            />
+        </td>
+    );
 }
 
 export default Cell;
