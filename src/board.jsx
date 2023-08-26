f5120a5cf4258463044899f3d4e4c83e313169f3
99061142 <xarbprive@gmail.com>
2023-08-26 19:13:18 +0200

    Read description
    Changes:
    * Function turned into class. With this change the useStates got changed into the class state and separated functions to set the state. The same goes for useEffect. This got changed into componentDidMount.
    
    * Every element property on it's own line to make the code more readable
    
    * Add every Cell component prop individually  instead of using {...props}.
    
    Deleted:
    * Cell component 'initWeight' prop.

--- src/board.jsx
+++ src/board.jsx
@@ -1,72 +1,101 @@
-import { createRef, useEffect, useState } from "react";
+import { Component, createRef } from 'react';
 import Cell from './cell';
 
-function Board(props) {
-    const [rows, setRows] = useState(0);
-    const [cols, setCols] = useState(0);
-    const board = createRef(null);
+class Board extends Component {
+    constructor() {
+        super();
+        this.ref = createRef(null);
+        this.state = {
+            rows: 0,
+            cols: 0
+        };
+    }
 
-    useEffect(() => {
-        const maxRows = () => {
-            // Get the max amount of rows by the max height of the board divided by the cell view height
-            const BOARD = board.current;
-            const TOP = BOARD.getBoundingClientRect().top;
-            const MAX_HEIGHT = (window.innerHeight - TOP);
-            const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
-            const ROWS = Math.floor(MAX_HEIGHT / CELL_PADDING) - 1;
-            return ROWS
-        }
-        setRows(maxRows());
+    setRows(rows) {
+        this.setState({
+            rows
+        });
+    }
 
-        const maxCols = () => {
-            // Get the max amount of cols by the max width of the board divided by the cell view height
-            const MAX_WIDTH = window.innerWidth;
-            const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
-            const COLS = Math.floor(MAX_WIDTH / CELL_PADDING) - 1;
-            return COLS
-        }
-        setCols(maxCols());
+    setCols(cols) {
+        this.setState({
+            cols
+        });
+    }
 
-        // eslint-disable-next-line
-    }, []);
+    maxRows() {
+        // Get the max amount of rows by the max height of the board divided by the cell view height
+        const BOARD = this.ref.current;
+        const TOP = BOARD.getBoundingClientRect().top;
+        const MAX_HEIGHT = (window.innerHeight - TOP);
+        const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
+        const ROWS = Math.floor(MAX_HEIGHT / CELL_PADDING) - 1;
+        return ROWS
+    }
 
-    const initCellType = (row, col) => {
-        // Return the type of the cell based on the row and col
-        const CENTER_ROW = Math.floor(rows / 2);
+    maxCols() {
+        // Get the max amount of columns by the max width of the board divided by the cell view height
+        const MAX_WIDTH = window.innerWidth;
+        const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
+        const COLS = Math.floor(MAX_WIDTH / CELL_PADDING) - 1;
+        return COLS
+    }
+
+    componentDidMount() {
+        // Set the rows/cols to initialize the board
+        const MAX_ROWS = this.maxRows()
+        this.setRows(MAX_ROWS);
+        const MAX_COLS = this.maxCols()
+        this.setCols(MAX_COLS);
+    }
+
+    initCellType(row, col) {
+        // Return the initialized type of the cell based on the row and column 
+        const CENTER_ROW = Math.floor(this.state.rows / 2);
         if (row !== CENTER_ROW) {
             return ''
         }
-        const START_COL = Math.floor(cols * .15);
+        const START_COL = Math.floor(this.state.cols * .15);
         if (col === START_COL) {
             return 'start'
         }
-        const END_COL = Math.floor(cols * .85);
+        const END_COL = Math.floor(this.state.cols * .85);
         if (col === END_COL) {
             return 'end'
         }
         return ''
     }
 
-    return (
-        <table ref={board} className="my-2 d-flex justify-content-center">
-            <tbody>
-                {[...Array(rows)].map((_, row) =>
-                    <tr key={row}>
-                        {[...Array(cols)].map((_, col) =>
-                            <Cell
-                                initWeight={1}
-                                initType={initCellType(row, col)}
-                                row={row}
-                                col={col}
-                                key={col}
-                                {...props}
-                            />
-                        )}
-                    </tr>
-                )}
-            </tbody>
-        </table>
-    );
+    render() {
+        return (
+            <table
+                ref={this.ref}
+                className='my-2 d-flex justify-content-center'
+            >
+                <tbody>
+                    {[...Array(this.state.rows)].map((_, row) =>
+                        <tr
+                            key={row}
+                        >
+                            {[...Array(this.state.cols)].map((_, col) =>
+                                <Cell
+                                    type={this.initCellType(row, col)}
+                                    key={col}
+                                    row={row}
+                                    col={col}
+                                    running={this.props.running}
+                                    setRunning={this.props.setRunning}
+                                    addCellToBoard={this.props.addCellToBoard}
+                                    setStartPos={this.props.setStartPos}
+                                    setEndPos={this.props.setEndPos}
+                                />
+                            )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        )
+    }
 }
 
 export default Board;
