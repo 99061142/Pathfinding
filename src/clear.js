23c3e1bee697472beea95238fdd5cbdde5d4fe4b
99061142 <xarbprive@gmail.com>
2023-08-26 19:27:43 +0200

    Use the 'board' dict to get/set cell data
    Changed:
    * Use the parameter 'board' dict to get/set the cell data.
    
    Added:
    * ClearWalls function: If the wall gets cleared, set the weight to 1
    
    * ClearPath function: changed cell type 'next' to 'queued' inside the PATH_TYPES list.(Cell types that needs to be cleared).

--- src/clear.js
+++ src/clear.js
@@ -1,39 +1,51 @@
-const getCells = () => document.querySelectorAll('td');
-
-export async function ClearAll() {
-    for (const cell of getCells()) {
-        const CELL_TYPE = cell.dataset.type;
-        const CELL_WEIGHT = Number(cell.dataset.weight);
-        if ((CELL_TYPE === '' && CELL_WEIGHT === 1) || CELL_TYPE === 'start' || CELL_TYPE === 'end') { continue }
-        
-        if(CELL_TYPE !== '') {
-            cell.dataset.type = '';
+export async function ClearAll(board) {
+    for (let row of board) {
+        for(let cell of row) {
+            const CELL_TYPE = cell.getType()
+            if(CELL_TYPE === 'start' || CELL_TYPE === 'end') { 
+                continue 
+            }
+            if(CELL_TYPE !== '') {
+                cell.setType('');
+            }
+            if(cell.getWeight() !== 1) {
+                cell.setWeight(1);
+            }
         }
-        if(CELL_WEIGHT !== 1) {
-            cell.dataset.weight = 1;
+    }
+}
+
+export async function ClearPath(board) {
+    const PATH_TYPES = ['visited', 'queued', 'fastest'];
+    for (let row of board) {
+        for(let cell of row) {
+            if (!PATH_TYPES.includes(cell.getType())) { 
+                continue 
+            }
+            cell.setType('');
         }
     }
 }
 
-export async function ClearPath() {
-    const PATH_TYPES = ["visited", "next", "fastest"];
-    for (const cell of getCells()) {
-        if (!PATH_TYPES.includes(cell.dataset.type)) { continue }
-        cell.dataset.type = '';
+export async function ClearWalls(board) {
+    for (let row of board) {
+        for(let cell of row) {
+            if (cell.getType() !== 'wall') { 
+                continue
+            }
+            cell.setType('');
+            cell.setWeight(1) 
+        }
     }
 }
 
-export async function ClearWalls() {
-    for (const cell of getCells()) {
-        if (cell.dataset.type !== 'wall') { continue }
-        cell.dataset.type = '';
-    }
-}
-
-export async function ClearWeights() {
-    for (const cell of getCells()) {
-        const CELL_WEIGHT = Number(cell.dataset.weight)
-        if (CELL_WEIGHT === 1) { continue }
-        cell.dataset.weight = 1;
+export async function ClearWeights(board) {
+    for (let row of board) {
+        for(let cell of row) {
+            if (cell.getWeight() === 1) { 
+                continue
+            }
+            cell.setWeight(1);
+        }
     }
 }
