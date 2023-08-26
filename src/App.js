7df404c98052ed2ecfa0bd3d63b5388b28af85b9
99061142 <xarbprive@gmail.com>
2023-08-26 19:03:12 +0200

    Read description for (added) changes
    Changed:
    * 'app.css' import got changed to 'app.scss'.
    
    * Function got changed into a class.
    
    * running/ setRunning state got changed into placing 'running' inside the class state, and setRunning as arrow function.
    
    * Instead of giving the settings/board components the same props, only give the component the props needed.
    
    Added:
    * Added board, startPos and endPos to the state.
    With the board state you can get and set every state of the cell.
    
    * Added setStartPos as arrow function
    
    * Added setEndPos as arrow function
    
    * addCellToBoard function to add the cell data to the board state (inside the board row given as parameter).

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import './app.css';
-import { useState } from 'react';
+import './css/app.scss';
+import { Component  } from 'react';
 import Settings from './settings';
 import CellInformation from './cellInformation';
 import Board from './board';
 
-function App() {
-    const [running, setRunning] = useState(false);
-    const PROPS = {
-        running,
-        setRunning
-    };
-    return (
-        <>
-            <Settings {...PROPS} />
-            <CellInformation />
-            <Board {...PROPS} />
-        </>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            running: false,
+            board: [],
+            startPos: null,
+            endPos: null
+        };
+    }
+
+    setStartPos = (pos) => {
+        this.setState({
+            startPos: pos
+        });
+    }
+
+    setEndPos = (pos) => {
+        this.setState({
+            endPos: pos
+        });
+    }
+    
+    setRunning = (bool) => {
+        this.setState({
+            running: bool
+        });
+    }
+
+    addCellToBoard = (row, cell) => {
+        let board = this.state.board
+        if (row >= board.length) {
+            board.push([])
+        }
+        board[row].push(cell)
+
+        this.setState({
+            board
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <Settings 
+                    setRunning={this.setRunning}
+                    board={this.state.board}
+                    startPos={this.state.startPos}
+                    endPos={this.state.endPos}
+                    running={this.state.running}
+                />
+                <CellInformation />
+                <Board 
+                    addCellToBoard={this.addCellToBoard} 
+                    setStartPos={this.setStartPos}
+                    setEndPos={this.setEndPos}
+                    running={this.state.running}
+                />
+            </>
+        );
+    }
 }
 
 export default App;
