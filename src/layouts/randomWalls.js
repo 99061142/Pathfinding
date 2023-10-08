deed203650c55fa90b49a5a22b4abd2fca9bad27
99061142 <xarbprive@gmail.com>
2023-10-08 20:29:21 +0200

    Render dropdown item inside the class
    Changed the function to a class, and rendered the dropdown item inside this class instead of the "Settings" class.

--- src/layouts/randomWalls.js
+++ src/layouts/randomWalls.js
@@ -1,17 +1,40 @@
+// Imports to render the component
+import { Component } from "react";
+import { Button } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+
+// Import to clear the board cells
 import { ClearAll } from "../clear";
 
-async function RandomWalls(board) {
-    // CLear the board before creating a new layout
-    await ClearAll(board);
+class RandomWalls extends Component {
+    async clicked() {
+        // CLear the board before creating a new layout
+        const BOARD = this.props.board;
+        await ClearAll(BOARD);
 
-    for (let row of board) {
-        for(let cell of row) {
-            if(cell.getType() !== '' || Math.random() > .15) { 
-                continue
+        // Create a new layout with random walls
+        for (let row of BOARD) {
+            for(let cell of row) {
+                // If the cell is already filled, or the random % is higher than 15%, continue
+                if(cell.getType() !== '' || Math.random() > .15) { 
+                    continue
+                }
+                // Set the cell as wall
+                cell.setType('wall');
+                cell.setWeight(Infinity);
             }
-            cell.setType('wall');
-            cell.setWeight(Infinity);
         }
+    }
+
+    render() {
+        return (
+            <DropdownItem
+                as={Button}
+                onClick={() => this.clicked()}
+            >
+                Random walls
+            </DropdownItem>
+        );
     }
 }
 
