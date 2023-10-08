ecac8fa9718e82ee0a0313065625611bbfdf37e6
99061142 <xarbprive@gmail.com>
2023-10-08 20:41:35 +0200

    Added comments / removed unnecessary if statements

--- src/clear.js
+++ src/clear.js
@@ -1,27 +1,28 @@
 export async function ClearAll(board) {
     for (let row of board) {
         for(let cell of row) {
-            const CELL_TYPE = cell.getType()
-            if(CELL_TYPE === 'start' || CELL_TYPE === 'end') { 
+            // If the cell is the start or end position, continue
+            const CELL_TYPE = cell.getType();
+            if(CELL_TYPE === "start" || CELL_TYPE === "end") { 
                 continue 
             }
-            if(CELL_TYPE !== '') {
-                cell.setType('');
-            }
-            if(cell.getWeight() !== 1) {
-                cell.setWeight(1);
-            }
+            // Clear the cell
+            cell.setType('');
+            cell.setWeight(1);
         }
     }
 }
 
 export async function ClearPath(board) {
-    const PATH_TYPES = ['visited', 'queued', 'fastest'];
+    const PATH_TYPES = ["visited", "queued", "fastest"];
     for (let row of board) {
         for(let cell of row) {
-            if (!PATH_TYPES.includes(cell.getType())) { 
-                continue 
+            // If the cell type isn't one of the values inside PATH_TYPES, continue
+            const CELL_TYPE = cell.getType();
+            if (!PATH_TYPES.includes(CELL_TYPE)) { 
+                continue
             }
+            // Clear the cell
             cell.setType('');
         }
     }
@@ -30,11 +31,14 @@ export async function ClearPath(board) {
 export async function ClearWalls(board) {
     for (let row of board) {
         for(let cell of row) {
-            if (cell.getType() !== 'wall') { 
+            // If the cell type isn't "weight", continue
+            const CELL_TYPE = cell.getType();
+            if (CELL_TYPE !== "wall") { 
                 continue
             }
+            // Clear the cell
             cell.setType('');
-            cell.setWeight(1) 
+            cell.setWeight(1);
         }
     }
 }
@@ -42,10 +46,10 @@ export async function ClearWalls(board) {
 export async function ClearWeights(board) {
     for (let row of board) {
         for(let cell of row) {
-            if (cell.getWeight() === 1) { 
-                continue
+            // If the cell is weighted, clear the cell
+            if (cell.getWeight() === 10) { 
+                cell.setWeight(1);
             }
-            cell.setWeight(1);
         }
     }
 }
