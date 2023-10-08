6757d02e46cb29f9e7db997efb2354f96384fd15
99061142 <xarbprive@gmail.com>
2023-10-08 20:30:04 +0200

    Update randomWeights.js
    Changed the function to a class, and rendered the dropdown item inside this class instead of the "Settings" class.

--- src/layouts/randomWeights.js
+++ src/layouts/randomWeights.js
@@ -1,16 +1,40 @@
+// Imports to render the component
+import { Component } from "react";
+import { Button } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+
+// Import to clear the board cells
 import { ClearAll } from "../clear";
 
-async function RandomWeights(board) {
-    // CLear the board before creating a new layout
-    await ClearAll(board);
+class RandomWeights extends Component {
+    async clicked() {
+        // CLear the board before creating a new layout
+        const BOARD = this.props.board;
+        await ClearAll(BOARD);
 
-    for (let row of board) {
-        for(let cell of row) {
-            if(cell.getType() !== '' || Math.random() > .15) { 
-                continue
+        // Create a new layout with random weights
+        for (let row of BOARD) {
+            for(let cell of row) {
+                // If the cell is already filled, or the random % is higher than 15%, continue
+                if(cell.getType() !== '' || Math.random() > .15) { 
+                    continue
+                }
+                // Set the cell as wall
+                cell.setWeight(10);
             }
-            cell.setWeight(10);
         }
+    }
+
+    render() {
+        return (
+            <DropdownItem
+                as={Button}
+                onClick={() => this.clicked()}
+                disabled={!this.props.algorithmWeighted}
+            >
+                Random weights
+            </DropdownItem>
+        );
     }
 }
 
