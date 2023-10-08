a1ddbc31c12c94e6b30b50e544871a25f391f83a
99061142 <xarbprive@gmail.com>
2023-10-08 20:33:28 +0200

    Removed dataset "type"
    Removed dataset "type" and only add the cell type inside the classNames if it isn't "weight".

--- src/cellInformation.jsx
+++ src/cellInformation.jsx
@@ -1,27 +1,27 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 function CellInformation() {
-    const TYPES = ['start', 'end', 'weight', 'wall', 'visited', 'queued', 'fastest'];
+    // Show the user every type of cell with the given SVG (if the type has an SVG)
+    const TYPES = ["start", "end", "weight", "wall", "visited", "queued", "fastest"];
     return (
         <Container
-            className='my-3 d-none d-sm-block'
+            className="my-3 d-none d-sm-block"
             fluid
         >
             <Row
-                className='justify-content-around text-center'
+                className="justify-content-around text-center"
             >
                 {TYPES.map((type, key) =>
                     <Col
                         key={key}
                     >
                         <div
-                            className='cell mx-auto'
-                            data-type={type}
-                            data-weight={type === 'weight' ? 10 : 1}
+                            className={"cell mx-auto" + (type === "weight" ? '' : ' ' + type)}
+                            data-weight={type === "weight" ? 10 : 1}
                         >
                         </div>
                         <p
-                            className='mb-0 text-capitalize'
+                            className="mb-0 text-capitalize"
                         >
                             {type}
                         </p>
