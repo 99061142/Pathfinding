0cffa87d27f9efcac799905d9d9ba913943317c2
99061142 <xarbprive@gmail.com>
2023-12-09 22:59:01 +0100

    Update App.js
    ADDED:
    * Settings component ref (this.settings).
    
    * Board component ref (this.board).
    
    * board ref to the Settings component
    
    * settings ref to the Board component.
    
    * board ref to the Board component.
    
    CHANGED:
    * CellInformation import / component to CellExamples
    
    REMOVED:
    * pencilType state.
    
    * pencilWeight state.
    
    * running state.
    
    * board state.
    
    * startPos state.
    
    * endPos state.
    
    * setPencilType function. (Function to set the pencilType state.)
    
    * setStartPos function. (Function to set the startPos state.)
    
    * setEndPos function. (Function to set the endPos state.)
    
    * setRunning function. (Function to set the running state.)
    
    * addCellToBoard function. (Function to add the setters and getters of the cell to the board state.)
    
    * setRunning, setPencilType, setPencilWeight, board, startPos, endPos, running and pencilType props to the Settings component.
    
    * addCellToBoard, setStartPos, setEndPos, running, pencilType, pencilWeight props to the Board component.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,27 @@
-import { Component } from 'react';
-import Settings from './settings';
-import CellInformation from './cellInformation';
-import Board from './board';
+import { Component, createRef } from 'react';
+import Settings from './Settings';
+import CellExamples from './CellExamples';
+import Board from './Board';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            pencilType: 'wall',
-            pencilWeight: Infinity,
-            running: false,
-            board: [],
-            startPos: null,
-            endPos: null
-        };
+        this.settings = createRef(null);
+        this.board = createRef(null);
     }
-
-    setPencilType = (type) => {
-        this.setState({
-            pencilType: type
-        });
-    }
-
-    setPencilWeight = (weight) => {
-        this.setState({
-            pencilWeight: weight
-        });
-    }
-
-    setStartPos = (pos) => {
-        this.setState({
-            startPos: pos,
-        });
-    };
-
-    setEndPos = (pos) => {
-        this.setState({
-            endPos: pos,
-        });
-    };
-
-    setRunning = (bool) => {
-        this.setState({
-            running: bool,
-        });
-    };
-
-    addCellToBoard = (row, cell) => {
-        // If the length of the board list is lower than the cell row, add a new row to the board
-        let board = this.state.board;
-        if (row >= board.length) {
-            board.push([]);
-        }
-
-        // Add the cell to the row
-        board[row].push(cell);
-        this.setState({
-            board
-        });
-    };
 
     render() {
         return (
             <>
                 <Settings
-                    setRunning={this.setRunning}
-                    setPencilType={this.setPencilType}
-                    setPencilWeight={this.setPencilWeight}
-                    board={this.state.board}
-                    startPos={this.state.startPos}
-                    endPos={this.state.endPos}
-                    running={this.state.running}
-                    pencilType={this.state.pencilType}
+                    board={this.board}
+                    ref={this.settings}
                 />
-                <CellInformation />
+                <CellExamples />
                 <Board
-                    addCellToBoard={this.addCellToBoard}
-                    setStartPos={this.setStartPos}
-                    setEndPos={this.setEndPos}
-                    running={this.state.running}
-                    pencilType={this.state.pencilType}
-                    pencilWeight={this.state.pencilWeight}
+                    settings={this.settings}
+                    board={this.board}
+                    ref={this.board}
                 />
             </>
         );
