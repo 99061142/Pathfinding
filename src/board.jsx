b5f31f567c49b29fe0904aaac640c994a8d6e48a
99061142 <xarbprive@gmail.com>
2023-12-10 01:09:19 +0100

    Read description for additions and changes
    ADDED:
    * cells state (2d list with references to the Cell components).
    
    * init function. (Set the cells list with references when the component is mounted to callback when needed.)
    
    * startPos class var. (Set when component is mounted  & when the user moves the start pos.)
    
    * endPos class var. (Set when component is mounted  & when the user moves the end pos.)
    
    * clearWalls function. (Clear all the walls on the board.)
    
    * clearPath function. (Clear the path on the board.)
    
    * clearWeights function. (Clear all the weights on the board.)
    
    * clearAll function. (Clear everything on the board except the start & end position.)
    
    * randomWalls function. (Place random walls on the board.)
    
    * randomWeights function. (Place random weights on the board.)
    
    * setter for the cells.
    
    * hasPath getter. (Return if there is a path on the board.)
    
    CHANGED:
    * rows state changed to rows class var.
    
    * cols state changed to rowCols class var.
    
    * ref class var renamed to tableRef.

--- src/board.jsx
+++ src/board.jsx
@@ -1,103 +1,213 @@
 import { Component, createRef } from 'react';
-import Cell from './cell';
+import Cell from './Cell';
 
 class Board extends Component {
     constructor() {
         super();
-        this.ref = createRef(null);
         this.state = {
-            rows: 0,
-            cols: 0
+            cells: []
         };
-    }
-
-    setRows(rows) {
-        this.setState({
-            rows
-        });
-    }
-
-    setCols(cols) {
-        this.setState({
-            cols
-        });
-    }
-
-    maxRows() {
-        // Get the max amount of rows by the max height of the board divided by the cell view height
-        const BOARD = this.ref.current;
-        const TOP = BOARD.getBoundingClientRect().top;
-        const MAX_HEIGHT = (window.innerHeight - TOP);
-        const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
-        const ROWS = Math.floor(MAX_HEIGHT / CELL_PADDING) - 1;
-        return ROWS
-    }
-
-    maxCols() {
-        // Get the max amount of columns by the max width of the board divided by the cell view height
-        const MAX_WIDTH = window.innerWidth;
-        const CELL_PADDING = ((window.innerHeight / 100) * 1.75) * 2;
-        const COLS = Math.floor(MAX_WIDTH / CELL_PADDING) - 1;
-        return COLS
+        this.rows = 0; // Set when component is mounted (need tableRef)
+        this.rowCols = this.maxRowCols;
+        this.startPos = null;
+        this.endPos = null;
+        this.tableRef = createRef(null);
     }
 
     componentDidMount() {
-        // Set the rows/cols to initialize the board
-        const MAX_ROWS = this.maxRows()
-        this.setRows(MAX_ROWS);
-        const MAX_COLS = this.maxCols()
-        this.setCols(MAX_COLS);
+        this.rows = this.maxRows;
+
+        // Load the board
+        this.init();
     }
 
-    initCellType(row, col) {
-        // Return the initialized type of the cell based on the row and column
-        const CENTER_ROW = Math.floor(this.state.rows / 2);
-        if (row !== CENTER_ROW) {
-            return ''
+    init() {
+        // Create a 2d list with references that can be used to callback a cell component
+        // and set the created 2d list to the board state
+        const cells = []
+        for (let row = 0; row < this.rows; row++) {
+            cells.push([]);
+            for (let col = 0; col < this.rowCols; col++) {
+                cells[row].push(createRef(null));
+            }
         }
-        const START_COL = Math.floor(this.state.cols * .15);
-        if (col === START_COL) {
-            return "start"
-        }
+        this.cells = cells;
+    }
 
-        const END_COL = Math.floor(this.state.cols * .75);
-        if (col === END_COL) {
-            return "end"
+    get cells() {
+        const CELLS = this.state.cells;
+        return CELLS
+    }
+
+    set cells(cells) {
+        this.setState({
+            cells
+        });
+    }
+
+    get hasPath() {
+        const HAS_PATH = document.querySelector('.cell.visited, .cell.queued, .cell.route');
+        return HAS_PATH
+    }
+
+    clearWalls() {
+        // Clear all the walls
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                const cell = col.current;
+                if (cell.type !== "wall") continue
+                cell.type = '';
+                cell.weight = 1;
+            }
         }
-        return ''
+    }
+
+    clearWeights() {
+        // Clear all the weights
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                const cell = col.current;
+                if (!cell.type.includes('weight')) continue
+                cell.type = '';
+                cell.weight = 1;
+            }
+        }
+    }
+
+    clearPath() {
+        const PATH_TYPES = [
+            "visited",
+            "queued",
+            "route"
+        ];
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                const cell = col.current;
+                for (const pathType of PATH_TYPES) {
+                    // If the cell is weighted AND has a path type ( type = weight-[path type] ) set the cell type to 'weight' 
+                    // else if the cell type is only a path type ( type = [path type] ) set the cell type to an empty string
+                    if (cell.type.includes(pathType)) cell.type = cell.type.includes('weight') ? "weight" : ''
+                }
+            }
+        }
+    }
+
+    clearAll() {
+        // Clear all the cells except the start and end position
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                const cell = col.current;
+                if (
+                    cell.type !== "start" &&
+                    cell.type !== "end"
+                ) {
+                    cell.type = '';
+                    cell.weight = 1;
+                }
+            }
+        }
+    }
+
+    async randomWalls() {
+        // CLear the board before creating a new layout
+        this.clearAll();
+
+        // Create a new layout with random walls
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                // Set the cell to a weight if the random % is lower than 25% and the cell isn't the start or end position
+                const cell = col.current;
+                if (
+                    Math.random() < .25 &&
+                    cell.type !== "start" &&
+                    cell.type !== "end"
+                ) {
+                    cell.type = "wall";
+                    cell.weight = Infinity;
+                }
+            }
+        }
+    }
+
+    async randomWeights() {
+        // CLear the board before creating a new layout
+        this.clearAll();
+
+        // Create a new layout with random weights
+        for (const row of this.state.cells) {
+            for (const col of row) {
+                // Set the cell to a wall if the random % is lower than 25% and the cell isn't the start or end position
+                const cell = col.current;
+                if (
+                    Math.random() < .25 &&
+                    cell.type !== "start" &&
+                    cell.type !== "end"
+                ) {
+                    cell.type = "weight";
+                    cell.weight = 10;
+                }
+            }
+        }
+    }
+
+    get maxRows() {
+        // The calculation for the max rows of the board is as followed:
+        // ( window inner height - starting height of the table ) / cell height - 1
+        const CELL_HEIGHT = window.innerHeight / 100 * 3.5;
+        const TABLE_REF = this.tableRef.current;
+        const BOARD_TOP = TABLE_REF.getBoundingClientRect().top;
+        const MAX_BOARD_HEIGHT = window.innerHeight - BOARD_TOP;
+        const MAX_ROWS = Math.floor(MAX_BOARD_HEIGHT / CELL_HEIGHT) - 1;
+        return MAX_ROWS
+    }
+
+    get maxRowCols() {
+        // The calculation for the max row columns of the board is as followed:
+        // window inner width / cell width - 1
+        const CELL_WIDTH = window.innerWidth / 100 * 1.75;
+        const MAX_WIDTH = window.innerWidth;
+        const MAX_ROW_COLS = Math.floor(MAX_WIDTH / CELL_WIDTH) - 1;
+        return MAX_ROW_COLS
     }
 
     render() {
+        const cellInitType = (row, col) => {
+            // Return the initialized type of the cell based on the row and column
+            const CENTER_ROW = Math.floor(this.rows / 2);
+            if (row !== CENTER_ROW) return ''
+
+            const START_COL = Math.floor(this.rowCols * .15);
+            if (col === START_COL) return "start"
+
+            const END_COL = Math.floor(this.rowCols * .75);
+            if (col === END_COL) return "end"
+            return ''
+        }
+
         return (
             <table
-                ref={this.ref}
+                ref={this.tableRef}
                 className="my-2 d-flex justify-content-center"
             >
                 <tbody>
-                    {[...Array(this.state.rows)].map((_, row) =>
+                    {this.cells.map((rowCols, row) =>
                         <tr
                             key={row}
                         >
-                            {[...Array(this.state.cols)].map((_, col) =>
+                            {rowCols.map((ref, col) =>
                                 <Cell
-                                    initType={this.initCellType(row, col)}
+                                    initType={cellInitType(row, col)}
+                                    pos={[row, col]}
+                                    ref={ref}
                                     key={col}
-                                    row={row}
-                                    col={col}
-                                    running={this.props.running}
-                                    setRunning={this.props.setRunning}
-                                    addCellToBoard={this.props.addCellToBoard}
-                                    setStartPos={this.props.setStartPos}
-                                    setEndPos={this.props.setEndPos}
-                                    pencilType={this.props.pencilType}
-                                    pencilWeight={this.props.pencilWeight}
+                                    {...this.props}
                                 />
                             )}
                         </tr>
                     )}
                 </tbody>
-            </table >
-        )
+            </table>
+        );
     }
 }
 
