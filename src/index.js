8bdf2e57d19c4f31167399e42fc72306fde55431
99061142 <xarbprive@gmail.com>
2024-02-24 23:24:07 +0100

    Moved files to pathfindingVisualizer folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import App from './pathfindingVisualiser/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
